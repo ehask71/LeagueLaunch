5455aa308d7833c262f6552eeda20c364f335114
ehask71 <ehask71@gmail.com>
2013-05-05 23:21:07 -0400

    Copyright

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,6 +2,11 @@
 
 class UsersController extends AppController {
 
+    public function beforeFilter() {
+	parent::beforeFilter();
+	$this->Auth->allow();
+    }
+    
     public function login() {
 
     }
