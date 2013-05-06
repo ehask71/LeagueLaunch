551abe5f2f33aaef38078d54e512e2f796b88963
Eric Haskins <ehask71@gmail.com>
2013-05-06 01:49:17 -0400

    Adding CPanel post Acct Scripts. This will change to Hooks

--- app/Model/Settings.php
+++ app/Model/Settings.php
@@ -16,25 +16,6 @@ class Settings extends AppModel{
         )
     );
     
-    function getSiteSettings($siteid){
-	$sql = "SELECT name,value,type FROM settings WHERE site_id = '$siteid'";
-	$result = $this->query($sql);
-	$settings = array();
-	if(count($result)>0){
-	    foreach ($result as $row) {
-		if($row['type'] == 'object' || $row['type'] == 'array'){
-                    // Handle Arrays and Objects
-                    $settings[$row['name']] = unserialize($row['value']);
-                } else {
-                    // Default is string
-                    $settings[$row['name']] = (string)$row['value'];
-                }
-	    }
-	}
-	
-	return $settings;
-    }
-    
     function buildSettings($site,$setarray){
 	$settings = array();
 	if(count($setarray)>0){
@@ -53,6 +34,10 @@ class Settings extends AppModel{
 	    $settings['theme'] = 'default';
 	}
 	
+        foreach($settings as $key=>$value){
+            Configure::write("__".$key, $val);
+        }
+        
 	return $settings;
     }
 }
