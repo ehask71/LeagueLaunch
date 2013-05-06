a83aa479db2437d8e21e7b5f5eeb8d74016c1f3e
Eric Haskins <ehask71@gmail.com>
2013-05-06 02:26:01 -0400

    Adding CPanel post Acct Scripts. This will change to Hooks

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,48 +1,52 @@
 <?php
+
 App::uses('Controller', 'Controller');
 
 class AppController extends Controller {
-    public $viewClass   = 'Theme';
+
+    public $viewClass = 'Theme';
     public $theme = 'default';
-    public $uses = array('Settings','Sites');
-    
+    public $uses = array('Settings', 'Sites');
     public $components = array(
-		'Session',
-		'Auth'
+        'Session',
+        'Auth'
     );
 
     public function beforeFilter() {
-	$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
+        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
         $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
-	
-	$domain = $this->getDomain();
-	if($this->Sites->getSiteId($domain)){
-	    $result = $this->Sites->find('first',array(
-		'conditions' => array(
-		    'Sites.domain'=>$domain,
-		    'Sites.isactive'=>'yes')
-	    ));
-	    if(count($result)>0){
-		$settings = $this->Settings->buildSettings($result['Sites'],$result['Settings']);
-		$this->set('domain',$domain);
-		$this->set('settings',$settings);
-	    } else {
+
+        $domain = $this->getDomain();
+        if ($this->Sites->getSiteId($domain)) {
+            $result = $this->Sites->find('first', array(
+                'conditions' => array(
+                    'Sites.domain' => $domain,
+                    'Sites.isactive' => 'yes')
+                    ));
+            if (count($result) > 0) {
+                $settings = $this->Settings->buildSettings($result['Sites'], $result['Settings']);
+                $this->set('domain', $domain);
+                $this->set('settings', $settings);
+            } else {
                 $this->redirect("/notconfigured");
             }
-	}
+        } else {
+            $this->redirect("/notconfigured");
+        }
     }
-    
+
     public function beforeRender() {
-	parent::beforeRender();
+        parent::beforeRender();
     }
-    
-    public function getDomain(){
-	if(strpos($_SERVER['SERVER_NAME'], 'leaguelaunch.com')){
-	    $domain = str_replace('.leaguelaunch.com','',str_replace('www.', '', $_SERVER['SERVER_NAME']));
-	} else {
-	    $domain = str_replace('www.', '', $_SERVER['SERVER_NAME']);
-	}
-	
-	return $domain;
+
+    public function getDomain() {
+        if (strpos($_SERVER['SERVER_NAME'], 'leaguelaunch.com')) {
+            $domain = str_replace('.leaguelaunch.com', '', str_replace('www.', '', $_SERVER['SERVER_NAME']));
+        } else {
+            $domain = str_replace('www.', '', $_SERVER['SERVER_NAME']);
+        }
+
+        return $domain;
     }
+
 }
