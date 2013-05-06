c41255b642f2f5f0fbab6b12aeda98ddd465f2c6
Eric Haskins <ehask71@gmail.com>
2013-05-06 03:14:15 -0400

    Adding CPanel post Acct Scripts. This will change to Hooks

--- app/Model/Keywords.php
+++ app/Model/Keywords.php
@@ -6,8 +6,12 @@ class Keywords extends AppModel {
     public $useTable = 'keywords';
     
     public function getSportKeywords($sport){
-        $key = $this->find('all',array('conditions' => array('Keywords.sport' => $sport)));
-        
+        $key = $this->find('first',array('conditions' => array('Keywords.sport' => $sport)));
+        if(count($key)==0){
+            $key = 'baseball,football,rugby,tennis,track,volleyball,golf';
+        } else {
+            $key = $key['keywords'];
+        }
         return $key;
     }
 }
