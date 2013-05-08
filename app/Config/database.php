c590bcab56de8f81c401606855f272b860c047a8
Eric Haskins <ehask71@gmail.com>
2013-05-07 23:16:03 -0400

    Adding CPanel post Acct Scripts. This will change to Hooks

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,16 +1,48 @@
 <?php
+
 class DATABASE_CONFIG {
-public $default = array(
-'datasource' => 'Database/Mysql',
-'persistent' => false,
-'host' => 'localhost',
-'port' => '',
-'login' => 'root',
-'password' => '',
-'database' => 'league',
-'schema' => '',
-'prefix' => '',
-'encoding' => ''
-);
+
+    public $default = null;
+    
+    public $dev = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'port' => '',
+        'login' => 'root',
+        'password' => '',
+        'database' => 'league',
+        'schema' => '',
+        'prefix' => '',
+        'encoding' => ''
+    );
+    
+    public $prod = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'port' => '',
+        'login' => 'demoleag_site',
+        'password' => '070693cory',
+        'database' => 'demoleag_league',
+        'schema' => '',
+        'prefix' => '',
+        'encoding' => ''
+    );
+
+    function __construct() {
+        if (isset($_SERVER['CAKEDEV'])) {
+            switch ($_SERVER['CAKEDEV']) {
+                case 'ericmain':
+                    $this->default = $this->dev;
+                    break;
+                default:
+                    $this->default = $this->prod;
+                    break;
+            }
+        } else { 
+            $this->default = $this->prod;
+        }
+    }
 
 }
