2cc64d34e94589d142d62baeea1e6da0fa49a34f
Eric Haskins <ehask71@gmail.com>
2013-05-08 00:33:06 -0400

    Adding CPanel post Acct Scripts. This will change to Hooks

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -30,9 +30,13 @@ class AppController extends Controller {
                 $this->set('domain', $domain);
                 $this->set('settings', $settings);
             } else {
+                $this->set('meta_keywords','League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
+                $this->set('meta_description','LeagueLaunch.com :: League Management Made Easy');
                 throw new NotFoundException($domain.' Was not found or is misconfigured');
             }
         } else {
+            $this->set('meta_keywords','League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
+            $this->set('meta_description','LeagueLaunch.com :: League Management Made Easy');
             throw new NotFoundException($domain.' Was not found or is misconfigured');
         }
     }
