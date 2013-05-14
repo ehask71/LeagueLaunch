ffcffe7f0001799d65001b78710066e6b562e304
ehask71 <ehask71@gmail.com>
2013-05-14 02:16:16 -0400

    Custom Config CHanges

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -28,8 +28,8 @@ class AppController extends Controller {
 		$this->theme = $settings['theme'];
 		// Load Theme configs
 		Configure::config('theme', new PhpReader(APP . 'View' . DS . 'Themed' . DS . $this->theme . DS . 'Config'));
-		Configure::load($this->theme.'.conf', 'theme');
-		print_r(Configure::restore('Theme_config'));
+		//Configure::load($this->theme.'.conf', 'theme');
+		print_r(Configure::read('Theme_config'));
                 $this->set('meta_keywords',(!@$settings['meta_keywords']!='')?@$settings['meta_keywords']:'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
                 $this->set('meta_description',(!@$settings['meta_description']!='')?@$settings['meta_description']:'LeagueLaunch.com :: League Management Made Easy');
                 $this->set('domain', $domain);
