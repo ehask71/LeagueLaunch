d9a22196cb533f70b8212da44f9b81b4d508eab4
ehask71 <ehask71@gmail.com>
2013-05-14 01:45:31 -0400

    Theme Configs

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -29,7 +29,7 @@ class AppController extends Controller {
 		// Load Theme configs
 		Configure::config('theme', new PhpReader(APP . 'View' . DS . 'Themed' . DS . $this->theme . DS . 'Config'));
 		Configure::load($this->theme.'.conf', 'theme');
-		
+		print_r(Configure::restore('Theme_config'));
                 $this->set('meta_keywords',(!@$settings['meta_keywords']!='')?@$settings['meta_keywords']:'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
                 $this->set('meta_description',(!@$settings['meta_description']!='')?@$settings['meta_description']:'LeagueLaunch.com :: League Management Made Easy');
                 $this->set('domain', $domain);
