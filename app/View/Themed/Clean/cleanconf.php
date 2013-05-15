cec6c672c5b833c0d45dfb526638d60a3c9b4a18
Eric Haskins <ehask71@gmail.com>
2013-05-15 12:23:02 -0400

    Theme config fix

--- app/View/Themed/Clean/cleanconf.php
+++ app/View/Themed/Clean/cleanconf.php
@@ -1,7 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$config['Theme_Config'] = array(
+    'pages'=> array('index')
+);
 ?>
