896682ad792ab734814dd7fcda7b98c593f2f27f
ehask71 <ehask71@gmail.com>
2013-05-22 01:29:11 -0400

    NewsController

---
+++ app/Controller/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CakePHP NewsController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class NewsController extends AppController {
+
+    public function index(){
+        
+    }
+    
+}
