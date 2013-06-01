8f7faf99323854244aabb969a38743cf9927420a
Eric Haskins <ehask71@gmail.com>
2013-06-01 14:07:06 -0400

    Added acl.ini

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,6 +32,7 @@ class AppController extends Controller {
                     'Sites.isactive' => 'yes')
                     ));
             if (count($result) > 0) {
+                
                 $settings = $this->Settings->buildSettings($result['Sites'], $result['Settings']);
 		$this->theme = $settings['theme'];
                 // Override if we are admin!
@@ -42,11 +43,13 @@ class AppController extends Controller {
 		//echo APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme);
 		Configure::config('themeconfig', new PhpReader(APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme) . DS));
 		Configure::load($this->theme.'conf', 'themeconfig',true);
-		//print_r(Configure::read());
+                print_r($result['Sites']);
+		print_r(Configure::read());
                 $this->set('meta_keywords',(!@$settings['meta_keywords']!='')?@$settings['meta_keywords']:'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
                 $this->set('meta_description',(!@$settings['meta_description']!='')?@$settings['meta_description']:'LeagueLaunch.com :: League Management Made Easy');
                 $this->set('domain', $domain);
                 $this->set('settings', $settings);
+                $this->set('site_id',$result['Sites']);
             } else {
                 $this->set('meta_keywords','League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
                 $this->set('meta_description','LeagueLaunch.com :: League Management Made Easy');
