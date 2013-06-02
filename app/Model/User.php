a1e48ff21ee6f575597d971f0dccc241a37bbeaa
Eric Haskins <ehask71@gmail.com>
2013-06-02 10:00:22 -0400

    Refactored the Model to User

---
+++ app/Model/User.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * CakePHP Users
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+App::uses('AuthComponent', 'Controller/Component');
+
+class User extends AppModel {
+
+    public $primaryKey = 'id';
+
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        }
+        return true;
+    }
+
+}
+
