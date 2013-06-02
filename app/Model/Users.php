0629c4c78b6a9ef1edfed6bdecbf775c5752303d
ehask71 <ehask71@gmail.com>
2013-06-02 00:59:53 -0400

    Update

--- app/Model/Users.php
+++ app/Model/Users.php
@@ -4,8 +4,16 @@
  * @author Eric
  */
 App::uses('AppModel', 'Model');
+App::uses('AuthComponent', 'Controller/Component');
 
 class Users extends AppModel {
     public $primaryKey = 'id';
+    
+    public function beforeSave($options = array()) {
+    if (isset($this->data[$this->alias]['password'])) {
+        $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+    }
+    return true;
+}
 }
 
