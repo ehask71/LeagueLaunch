12a0c18463e49b832182778db691836aec687462
ehask71 <ehask71@gmail.com>
2013-06-03 23:53:45 -0400

    Update

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -22,7 +22,7 @@ class AppController extends Controller {
 
     public function beforeFilter() {
         $this->Auth->authorize = array('Tiny');
-        $this->Auth->authenticate = array('Form' => array('contain' => array('RolesUsers')));
+        $this->Auth->authenticate = array('Form' => array('contain' => array('RolesUsers'),'fields' => array('username' => 'email')));
 	$this->Widget->build($this->prefix,$this->params['controller'],$this->params['action']);
         print_r($this->Auth->user());
         $domain = $this->getDomain();
