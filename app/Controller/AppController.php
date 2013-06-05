6842dd385be5f6cd03a64281900f62671c6d5b58
ehask71 <ehask71@gmail.com>
2013-06-05 00:37:42 -0400

    test email auth

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -37,6 +37,7 @@ class AppController extends Controller {
 	    if (count($result) > 0) {
 		$this->Auth->authenticate = array(
 		    'Form' => array(
+			'fields' => array('username'=>'email'),
 			'conditions' => array('RolesUser.site_id' => $result['Sites']['site_id']),
 			'recursive' => 1,
 			//'contain' => array('RolesUser')
