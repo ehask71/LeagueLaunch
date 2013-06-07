13b766702ae002aff16287ae68899bcb250cae69
Eric Haskins <ehask71@gmail.com>
2013-06-07 02:25:09 -0400

    added /logout route

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -29,6 +29,7 @@
 	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 	Router::connect('/', array('controller' => 'home', 'action' => 'index'));
 	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
+        Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
         Router::connect('/terms', array('controller' => 'home', 'action' => 'terms'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
