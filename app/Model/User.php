25c83a2a1ac87901b71bc98e0d7bc0c5f85dccf6
Eric Haskins <ehask71@gmail.com>
2013-06-06 22:20:36 -0400

    Refactored the Model to User

--- app/Model/User.php
+++ app/Model/User.php
@@ -17,6 +17,7 @@ class User extends AppModel {
 	    'foreignKey' => 'user_id',
 	    'assosciationForeignKey' => 'role_id',
 	    'unique' => 'keepExisting',
+            'conditions' => array('RolesUsers.site_id'=>2)
 	)
     );
 
