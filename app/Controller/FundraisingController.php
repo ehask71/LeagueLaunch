2a9ec9f4b8da5e758809fa6e10d495e870255f10
Eric Haskins <ehask71@gmail.com>
2013-06-10 16:35:04 -0400

    SaaS modification for RolesUser.site_id condition

--- app/Controller/FundraisingController.php
+++ app/Controller/FundraisingController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP FundraisingController
  * @author Eric
@@ -6,26 +7,35 @@
 App::uses('AppController', 'Controller');
 
 class FundraisingController extends AppController {
-    
+
     public $name = 'Fundraising';
     public $uses = array('Fundraiser');
-    
+
     public function beforeFilter() {
 	parent::beforeFilter();
     }
-    
-    
-    public function index(){
-        
+
+    public function index() {
+	$fundraisers = $this->Fundraiser->find('all', array(
+	    'conditions' => array(
+		'Fundraiser.site_id' => Configure::read('Settings.site_id'),
+		'and' => array(
+		    'Fundraiser.start_date <= NOW()',
+		    'Fundraiser.end_date >= NOW()'
+		),
+		'Fundraiser.is_active' => 'yes')
+		));
+
+	$this->set('fundraisers', $fundraisers);
     }
-    
-    public function admin_index(){
-        $fundraisers = $this->Fundraiser->find('all', array(
-            'conditions' => array('Fundraiser.site_id'=>  Configure::read('Settings.site_id'))
-        ));
-        
-        $this->set('fundraisers',$fundraisers);
+
+    public function admin_index() {
+	$fundraisers = $this->Fundraiser->find('all', array(
+	    'conditions' => array('Fundraiser.site_id' => Configure::read('Settings.site_id'))
+		));
+
+	$this->set('fundraisers', $fundraisers);
     }
-    
+
 }
 
