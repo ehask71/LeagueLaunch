d1bab317842d70f924ed8b2e37007e85b74b8e7f
Eric Haskins <ehask71@gmail.com>
2013-06-15 18:28:24 -0400

    admin images

--- app/Controller/SitesController.php
+++ app/Controller/SitesController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP SitesController
  * @author Eric
@@ -6,36 +7,50 @@
 App::uses('AppController', 'Controller');
 
 class SitesController extends AppController {
-    
+
     public $name = 'Sites';
     public $uses = array('Settings');
-    
+
     public function beforeFilter() {
-	parent::beforeFilter();
+        parent::beforeFilter();
     }
-    
-    public function index(){
+
+    public function index() {
         $this->redirect('/');
     }
-    
-    public function admin_settings(){
-	$id = Configure::read('Settings.site_id');
-	$this->Settings->site_id = $id; 
-	if ($this->request->is('post')) {
-            if($this->request->data){
-                foreach ($this->request->data['Settings'] AS $key=>$var){
-                   $this->Settings->query("INSERT INTO settings SET value = '".addslashes($var)."',name='".$key."',site_id = ".Configure::read('Settings.site_id').
-                            " ON DUPLICATE KEY UPDATE
-                                value='".addslashes($var)."'");
-                        
+
+    public function admin_settings() {
+        $id = Configure::read('Settings.site_id');
+        $this->Settings->site_id = $id;
+        if ($this->request->is('post')) {
+            if ($this->request->data) {
+                if (is_array($this->request->data['Settings'])) {
+                    foreach ($this->request->data['Settings'] AS $key => $var) {
+                        $this->Settings->query("INSERT INTO settings SET value = '" . addslashes($var) . "',name='" . $key . "',site_id = " . Configure::read('Settings.site_id') .
+                                " ON DUPLICATE KEY UPDATE
+                                value='" . addslashes($var) . "'");
+                    }
                 }
+                $siteskeys = array('domain','leaguename','sport','organization','slogan','firstname','lastname','email','address');
+                if (is_array($this->request->data['Sites'])) {
+                    foreach ($this->request->data['Sites'] AS $key => $var) {
+                        $this->Settings->query("INSERT INTO sites SET value = '" . addslashes($var) . "',name='" . $key . "',site_id = " . Configure::read('Settings.site_id') .
+                                " ON DUPLICATE KEY UPDATE
+                                value='" . addslashes($var) . "'");
+                    }
+                }
+
                 $this->Session->setFlash('Settings Saved!');
                 //$this->redirect('/admin/sites/settings');
-            } 
-	}
-	$this->set('settings', $this->Settings->find('bysiteid'));
-        $this->set('sub',$this->request->data);
+            }
+        }
+        $this->set('settings', $this->Settings->find('bysiteid'));
+        $this->set('sub', $this->request->data);
     }
     
+    public function admin_account(){
+        
+    }
+
 }
 
