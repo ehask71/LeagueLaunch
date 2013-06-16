a57ec9a92035aad5dd4d4b55bec0dd1b32295b90
ehask71 <ehask71@gmail.com>
2013-06-16 00:01:05 -0400

    Sports Model

--- app/Model/Settings.php
+++ app/Model/Settings.php
@@ -1,65 +1,76 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * Description of TblSettings
  *
  * @author Eric
  */
-class Settings extends AppModel{
-    
-    public $findMethods = array('bysiteid' =>  true);
-    
+class Settings extends AppModel {
+
+    public $findMethods = array('bysiteid' => true);
     public $useTable = 'settings';
-    
     //public $actsAs = array('KeyValue');
-	
+
     public $belongsTo = array(
-        'Sites' => array(
-            'className'    => 'Sites',
-            'foreignKey'   => 'site_id'
-        )
+	'Sites' => array(
+	    'className' => 'Sites',
+	    'foreignKey' => 'site_id'
+	)
     );
-    
+
     protected function _findBysiteid($state, $query, $results = array()) {
-        if ($state === 'before') {
-            $query['conditions']['Settings.site_id'] = Configure::read('Settings.site_id');
-            return $query;
-        }
-        return $results;
+	if ($state === 'before') {
+	    $query['conditions']['Settings.site_id'] = Configure::read('Settings.site_id');
+	    return $query;
+	}
+	return $results;
     }
-    
-    public function buildSettings($site,$setarray){
+
+    public function buildSettings($site, $setarray) {
 	$settings = array();
-	if(count($setarray)>0){
+	if (count($setarray) > 0) {
 	    foreach ($setarray as $row) {
-		if($row['type'] == 'object' || $row['type'] == 'array'){
-                    // Handle Arrays and Objects
-                    $settings[$row['name']] = unserialize($row['value']);
-                } else {
-                    // Default is string
-                    $settings[$row['name']] = (string)$row['value'];
-                }
+		if ($row['type'] == 'object' || $row['type'] == 'array') {
+		    // Handle Arrays and Objects
+		    $settings[$row['name']] = unserialize($row['value']);
+		} else {
+		    // Default is string
+		    $settings[$row['name']] = (string) $row['value'];
+		}
 	    }
 	}
-	
-	if(!isset($settings['theme'])){
+
+	if (!isset($settings['theme'])) {
 	    $settings['theme'] = 'default';
 	}
-	
-        if(!isset($settings['meta_keywords'])){
-            App::import('model','Keywords');
-            $keywords = new Keywords();
-            $settings['meta_keywords'] = $keywords->getSportKeywords($site['sport']);
-        }
-        
-        foreach($settings as $key=>$value){
-            Configure::write("Settings.".$key, $value);
-        }
-        // Store Our Site_id for use elsewhere
-        Configure::write("Settings.site_id", $site['site_id']);
+
+	if (!isset($settings['meta_keywords'])) {
+	    App::import('model', 'Keywords');
+	    $keywords = new Keywords();
+	    $settings['meta_keywords'] = $keywords->getSportKeywords($site['sport']);
+	}
+
+	foreach ($settings as $key => $value) {
+	    Configure::write("Settings." . $key, $value);
+	}
+	// Store Our Site_id for use elsewhere
+	Configure::write("Settings.site_id", $site['site_id']);
 
 	return $settings;
     }
+
+    public function updateKeyVal($data) {
+	if (is_array($data->request->data['Settings'])) {
+	    foreach ($data->request->data['Settings'] AS $key => $var) {
+		$this->query("INSERT INTO settings SET value = '" . addslashes($var) . "',name='" . $key . "',site_id = " . Configure::read('Settings.site_id') .
+			" ON DUPLICATE KEY UPDATE
+                                value='" . addslashes($var) . "'");
+	    }
+	}
+    }
+
 }
 
 ?>
