194d051f02580e939ab89328299c34ab4ecee8d3
Eric Haskins <ehask71@gmail.com>
2013-06-17 12:52:12 -0400

    SaaS modification for RolesUser.site_id condition

--- app/Model/Divisions.php
+++ app/Model/Divisions.php
@@ -1,8 +1,23 @@
 <?php
+
 App::uses('AppModel', 'Model');
 
 class Divisions extends AppModel {
+
     public $primaryKey = 'division_id';
-    
+
+    public function divisionValidate() {
+	$validate1 = array(
+	    'leaguename' => array(
+		'mustNotEmpty' => array(
+		    'rule' => 'notEmpty',
+		    'message' => 'Please enter the League\'s Name')
+	    ),
+	);
+	
+	$this->validate = $validate1;
+        return $this->validates();
+    }
+
 }
 
