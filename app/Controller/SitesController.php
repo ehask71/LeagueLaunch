a4c41677ca623345a2d1e6e05f69761e175b16bc
Eric Haskins <ehask71@gmail.com>
2013-06-17 14:09:18 -0400

    SaaS modification for RolesUser.site_id condition

--- app/Controller/SitesController.php
+++ app/Controller/SitesController.php
@@ -36,7 +36,7 @@ class SitesController extends AppController {
         $siteid = Configure::read('Settings.site_id');
 	$this->set('countries',$this->Country->getCountries());
 	$this->set('sports',$this->Sports->getSports());
-        if ($this->request->is('post')) {
+        if ($this->request->isPut()) {
             $this->Sites->set($this->data);
             if ($this->Sites->siteValidate()) {
                 $this->Sites->save($this->request->data, false);
