f9879057ef0c8c3a3dc5c474a23af42be539c958
Eric Haskins <ehask71@gmail.com>
2013-06-17 11:45:03 -0400

    SaaS modification for RolesUser.site_id condition

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -44,16 +44,18 @@ class AppController extends Controller {
 		    )
 		);
 		$settings = $this->Settings->buildSettings($result['Sites'], $result['Settings']);
+		
+		// Set Theme From settings
 		$this->theme = $settings['theme'];
 		// Override if we are admin!
 		if (isset($this->params['prefix']) && $this->params['prefix'] == 'admin') {
 		    $this->theme = 'admin';
 		}
 		// Load Theme configs
-		//echo APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme);
 		Configure::config('themeconfig', new PhpReader(APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme) . DS));
 		Configure::load($this->theme . 'conf', 'themeconfig', true);
 
+		// Set Needed Data (meta, domain, etc)
 		$this->set('meta_keywords', (@$settings['meta_keywords'] != '') ? @$settings['meta_keywords'] : 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
 		$this->set('meta_description', (@$settings['meta_description'] != '') ? @$settings['meta_description'] : 'LeagueLaunch.com :: League Management Made Easy');
 		$this->set('domain', $domain);
