8c1d06bd2f57b7f667a02363bc2c63feeabeb263
Eric Haskins <ehask71@gmail.com>
2013-06-17 14:18:04 -0400

    SaaS modification for RolesUser.site_id condition

--- app/Controller/DivisionsController.php
+++ app/Controller/DivisionsController.php
@@ -22,19 +22,18 @@ class DivisionsController extends AppController {
 	if ($this->request->is('post')) {
 	    print_r($this->request->data);
 	    //$this->Divisions->set($this->data);
-            if ($this->Divisions->divisionValidate()) {
-                $this->Divisions->save($this->request->data, false);
-                $this->Session->setFlash(__('The Division was Added!'));
-                $this->redirect('/admin/divisions');
-            }
-	} else {
-	    $divisions = $this->Divisions->find('all', array(
-		'conditions' => array(
-		    'Divisions.site_id' => Configure::read('Settings.site_id')
-		)
-		    ));
-	    $this->set('divisions', $divisions);
+	    if ($this->Divisions->divisionValidate()) {
+		$this->Divisions->save($this->request->data, false);
+		$this->Session->setFlash(__('The Division was Added!'));
+		$this->redirect('/admin/divisions');
+	    }
 	}
+	$divisions = $this->Divisions->find('all', array(
+	    'conditions' => array(
+		'Divisions.site_id' => Configure::read('Settings.site_id')
+	    )
+		));
+	$this->set('divisions', $divisions);
     }
 
 }
