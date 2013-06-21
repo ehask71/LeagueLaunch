dbab329874f7dea306e43774dcf1ca2afe1084c6
Eric Haskins <ehask71@gmail.com>
2013-06-20 20:05:47 -0400

    admin images

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -12,7 +12,17 @@ class NewsController extends AppController {
     }
     
     public function index(){
-        
+        $this->paginate = array(
+	    'conditions' => array(
+		'News.site_id' => Configure::read('Settings.site_id')
+	    ) 
+	);
+        $news = $this->paginate('News');
+        if(!empty($this->request->params['requested'])){
+            return $news;
+        }
+        $this->set('title_for_layout','Latest News');
+        $this->set('news',$news);
     }
     
 }
