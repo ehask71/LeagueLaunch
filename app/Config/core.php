a6d49c03de0348136a4a4a18d761093f12b2492c
ehask71 <ehask71@gmail.com>
2013-06-23 16:37:59 -0400

    cache

--- app/Config/core.php
+++ app/Config/core.php
@@ -321,7 +321,7 @@ if (Configure::read('debug') > 0) {
 }
 
 // Prefix each application on the same server with a different string, to avoid Memcache and APC conflicts.
-$prefix = 'myapp_';
+$prefix = $_SERVER['SERVER_NAME'].'_';
 
 /**
  * Configure the cache used for general framework caching. Path information,
