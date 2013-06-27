eeb3c5ab6746ff26fda587e6081ad5fa5f5d0482
Eric Haskins <ehask71@gmail.com>
2013-06-27 11:25:04 -0400

    Hoebll

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,65 +1,68 @@
 <?php
 
 class UsersController extends AppController {
-    
-    public $uses = array('User','RoleUser');
+
+    public $uses = array('User', 'RoleUser');
     public $components = array('Email');
-    
+
     public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('login','register','forgetpwd','confirmpwd');
+	parent::beforeFilter();
+	$this->Auth->allow('login', 'register', 'forgetpwd', 'confirmpwd');
     }
 
     public function isAuthorized($user) {
-        if ($user['role'] == 'admin') {
-            return true;
-        }
-        if (in_array($this->action, array('edit', 'delete'))) {
-            if ($user['id'] != $this->request->params['pass'][0]) {
-                return false;
-            }
-        }
-        return true;
+	if ($user['role'] == 'admin') {
+	    return true;
+	}
+	if (in_array($this->action, array('edit', 'delete'))) {
+	    if ($user['id'] != $this->request->params['pass'][0]) {
+		return false;
+	    }
+	}
+	return true;
     }
 
     public function login() {
-        if ($this->request->is('post')) {
-            if ($this->Auth->login()) {
-                $this->redirect($this->Auth->redirect());
-            } else {
-                $this->Session->setFlash('Your username or password was incorrect.');
-            }
-        }
+	if ($this->request->is('post')) {
+	    if ($this->Auth->login()) {
+		$this->redirect($this->Auth->redirect());
+	    } else {
+		$this->Session->setFlash('Your username or password was incorrect.');
+	    }
+	}
     }
 
     public function register() {
-        if ($this->request->is('post')) {
-            if ($this->User->save($this->request->data)) {
-		// We need to store the site relation to the user we just generated
-		$userid = $this->User->getLastInsertID();
-		$this->loadModel('RoleUser');
-		$this->RoleUser->addUserSite($userid);
-		
-                $this->Session->setFlash('The user has been saved');
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash('The user could not be saved. Please, try again.');
-            }
-        }
+	if ($this->request->is('post')) {
+	    $this->User->set($this->data);
+	    if ($this->User->userValidate()) {
+		if ($this->User->save($this->request->data)) {
+		    // We need to store the site relation to the user we just generated
+		    $userid = $this->User->getLastInsertID();
+		    $this->loadModel('RoleUser');
+		    $this->RoleUser->addUserSite($userid);
+
+		    $this->Session->setFlash('The user has been saved');
+		    $this->redirect(array('action' => 'index'));
+		} else {
+		    $this->Session->setFlash('The user could not be saved. Please, try again.');
+		}
+	    }
+	}
     }
-    
+
     public function forgetpwd() {
-        if ($this->request->is('post')) {
-            
-        }
+	if ($this->request->is('post')) {
+	    
+	}
     }
-    
+
     public function confirmpwd() {
-        
+	
     }
-    
+
     public function logout() {
-        $this->redirect($this->Auth->logout());
+	$this->redirect($this->Auth->logout());
     }
 
 }
