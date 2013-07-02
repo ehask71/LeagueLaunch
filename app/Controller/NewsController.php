3c7fe8f2258852c908d7055d7d1747c7c17dce7d
ehask71 <ehask71@gmail.com>
2013-07-02 02:52:06 -0400

    cache

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -7,6 +7,8 @@ App::uses('AppController', 'Controller');
 
 class NewsController extends AppController {
 
+    public $uses = array('News');
+    
     public function beforeFilter() {
 	parent::beforeFilter();
         $this->Auth->allow('index');
@@ -28,6 +30,24 @@ class NewsController extends AppController {
         }
         $this->set('title_for_layout','Latest News');
         $this->set('news',$news);
+    }
+    
+    public function admin_index(){
+	if ($this->request->is('post')) {
+	    $this->News->set($this->data);
+	    if ($this->News->newsValidate()) {
+		$this->Divisions->save($this->request->data, false);
+		$this->Session->setFlash(__('The Division was Added!'),'default',array('class'=>'alert succes_msg'));
+		$this->redirect('/admin/divisions');
+	    }
+	}
+	$this->paginate = array(
+	    'conditions' => array(
+		'News.site_id' => Configure::read('Settings.site_id'),
+                'News.is_active' => 'yes'
+	    ) 
+	);
+        $news = $this->paginate('News');
     }
     
 }
