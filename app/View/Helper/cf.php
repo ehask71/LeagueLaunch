418a3c7025ad794a2d6ab8ad55d13d0a0dadce13
Eric Haskins <ehask71@gmail.com>
2013-07-05 14:43:37 -0400

    moved ckeditor to app/view/webroot

--- app/View/Helper/cf.php
+++ app/View/Helper/cf.php
@@ -20,7 +20,7 @@ class CfHelper extends AppHelper {
  * Possible options: 'assets.example.com', if you only have one host
  * Or: 'assets%d.example.com', if you have multiple hosts. %d gets replaced with host number
  */
-  private $assetHost = 'assets%d.example.com';
+  private $assetHost = 'cdn.leaguelaunch.com';
   
 /**
  * If above is 'assets%d.example.com' will generate host names from 0 - 3
