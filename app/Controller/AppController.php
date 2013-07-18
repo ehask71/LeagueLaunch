bc56685c45d262350ce62f888de25f94e5a269ed
Eric Haskins <ehask71@gmail.com>
2013-07-18 00:05:13 -0400

    jQuery Formbuilder Added

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -10,79 +10,112 @@ class AppController extends Controller {
     public $uses = array('Settings', 'Sites', 'Widget');
     public $helpers = array('CloudFlare');
     public $components = array(
-	'Session',
-	'Auth' => array(
-	    'authorize' => array('Tiny'),
-	    'authenticate' => array(
-		'all' => array('userModel' => 'Account'),
-		'Form' => array(
-		    'fields' => array('username' => 'email', 'password' => 'password'),
-		    'scope' => array(
-			//'RolesUser.site_id' => $result['Sites']['site_id'],
-			'Account.is_active' => 'yes'
-		    ),
-		    'recursive' => 1,
-		//'contain' => array('RolesUser')
-	    )),
-	    'loginRedirect' => array('controller' => 'home', 'action' => 'index'),
-	    'logoutRedirect' => array('controller' => 'account', 'action' => 'login'),
-	    'loginAction' => '/login',
-	)
+        'Session',
+        'Auth' => array(
+            'authorize' => array('Tiny'),
+            'authenticate' => array(
+                'all' => array('userModel' => 'Account'),
+                'Form' => array(
+                    'fields' => array('username' => 'email', 'password' => 'password'),
+                    'scope' => array(
+                        //'RolesUser.site_id' => $result['Sites']['site_id'],
+                        'Account.is_active' => 'yes'
+                    ),
+                    'recursive' => 1,
+                //'contain' => array('RolesUser')
+            )),
+            'loginRedirect' => array('controller' => 'home', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'account', 'action' => 'login'),
+            'loginAction' => '/login',
+        )
     );
 
     public function beforeFilter() {
-	//mail('ehask71@gmail.com','Test BF',print_r($this->params['controller'],1));
+        //mail('ehask71@gmail.com','Test BF',print_r($this->params['controller'],1));
 
-	$domain = $this->Sites->getDomain();
-	if ($this->Sites->getSiteId($domain)) {
-	    $result = $this->Sites->find('first', array(
-		'conditions' => array(
-		    'Sites.domain' => $domain,
-		    'Sites.isactive' => 'yes')
-		    ));
-	    if (count($result) > 0) {
-		$settings = $this->Settings->buildSettings($result['Sites'], $result['Settings']);
+        $domain = $this->Sites->getDomain();
+        if ($this->Sites->getSiteId($domain)) {
+            $result = $this->Sites->find('first', array(
+                'conditions' => array(
+                    'Sites.domain' => $domain,
+                    'Sites.isactive' => 'yes')
+                    ));
+            if (count($result) > 0) {
+                $settings = $this->Settings->buildSettings($result['Sites'], $result['Settings']);
 
-		// Set Theme From settings
-		$this->theme = $settings['theme'];
-		// Override if we are admin!
-		if (isset($this->params['prefix']) && $this->params['prefix'] == 'admin') {
-		    $this->theme = 'admin';
-		}
-		// Load Theme configs
-		Configure::config('themeconfig', new PhpReader(APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme) . DS));
-		Configure::load($this->theme . 'conf', 'themeconfig', true);
+                // Set Theme From settings
+                $this->theme = $settings['theme'];
+                // Override if we are admin!
+                if (isset($this->params['prefix']) && $this->params['prefix'] == 'admin') {
+                    $this->theme = 'admin';
+                }
+                // Load Theme configs
+                Configure::config('themeconfig', new PhpReader(APP . 'View' . DS . 'Themed' . DS . ucfirst($this->theme) . DS));
+                Configure::load($this->theme . 'conf', 'themeconfig', true);
 
-		// Load Widgets For Theme page
-		$this->Widget->build($this->prefix, $this->params['controller'], $this->params['action']);
-		// Make User Info avail to the View & Set a $loggedIn bool
-		$this->set('userinfo', $this->Auth->user());
-		$this->set('loggedIn', $this->Auth->loggedIn());
-		// Set Needed Data (meta, domain, etc)
-		$this->set('meta_keywords', (@$settings['meta_keywords'] != '') ? @$settings['meta_keywords'] : 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
-		$this->set('meta_description', (@$settings['meta_description'] != '') ? @$settings['meta_description'] : 'LeagueLaunch.com :: League Management Made Easy');
-		$this->set('domain', $domain);
-		$this->set('settings', $settings);
-		$this->set('site_id', $result['Sites']['site_id']);
-	    } else {
-		$this->set('meta_keywords', 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
-		$this->set('meta_description', 'LeagueLaunch.com :: League Management Made Easy');
-		throw new NotFoundException($domain . ' Was not found or is misconfigured');
-	    }
-	} else {
-	    $this->set('meta_keywords', 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
-	    $this->set('meta_description', 'LeagueLaunch.com :: League Management Made Easy');
-	    throw new NotFoundException($domain . ' Was not found or is misconfigured');
-	}
+                // Load Widgets For Theme page
+                $this->Widget->build($this->prefix, $this->params['controller'], $this->params['action']);
+                // Make User Info avail to the View & Set a $loggedIn bool
+                $this->set('userinfo', $this->Auth->user());
+                $this->set('loggedIn', $this->Auth->loggedIn());
+                // Set Needed Data (meta, domain, etc)
+                $this->set('meta_keywords', (@$settings['meta_keywords'] != '') ? @$settings['meta_keywords'] : 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
+                $this->set('meta_description', (@$settings['meta_description'] != '') ? @$settings['meta_description'] : 'LeagueLaunch.com :: League Management Made Easy');
+                $this->set('domain', $domain);
+                $this->set('settings', $settings);
+                $this->set('site_id', $result['Sites']['site_id']);
+            } else {
+                $this->set('meta_keywords', 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
+                $this->set('meta_description', 'LeagueLaunch.com :: League Management Made Easy');
+                throw new NotFoundException($domain . ' Was not found or is misconfigured');
+            }
+        } else {
+            $this->set('meta_keywords', 'League Launch,Sports Team management,League,Soccer,Baseball,Football,Hockey');
+            $this->set('meta_description', 'LeagueLaunch.com :: League Management Made Easy');
+            throw new NotFoundException($domain . ' Was not found or is misconfigured');
+        }
     }
 
     public function beforeRender() {
-	parent::beforeRender();
-	//$this->_setCommonWidgets($this->params);
+        parent::beforeRender();
+        //$this->_setCommonWidgets($this->params);
     }
 
-    function _setCommonWidgets($params) {
-	$this->set('latestNews', $this->Widget->retrieve('News', 'latest'));
+    public function _setCommonWidgets($params) {
+        $this->set('latestNews', $this->Widget->retrieve('News', 'latest'));
+    }
+
+    public function __unserializeFormStructure($formStructure) {
+        $formStructure = unserialize($formStructure);
+        $form = new Formbuilder($formStructure);
+        $form_structure = $form->getArrayFormStructure();
+
+        $FormStructure = null;
+        $FormLabel = null;
+        if (!empty($form_structure['form_structure'])) {
+            $y = 0;
+            foreach ($form_structure['form_structure'] as $idx => $fs):
+                if (in_array($fs['cssClass'], array('tabbable', 'section_break'))):
+                    continue;
+                endif;
+
+                if (in_array($fs['cssClass'], array('radio', 'checkbox', 'select'))):
+                    //$FormStructure[] = Set::extract('/value', $fs['values']);//array($fs['cssClass'] => Set::extract('/value', $fs['values']));
+                    foreach ($fs['values'] as $key => $value) {
+                        $FormStructure[$y][$key] = $value['value'];
+                    }
+
+                    $FormLabel[$idx] = $content = html_entity_decode($fs['title'], ENT_COMPAT, "UTF-8");
+                else:
+                    $FormStructure[] = $fs['cssClass'];
+                    $FormLabel[] = html_entity_decode($fs['values'], ENT_COMPAT, "UTF-8");
+                endif;
+
+                $y++;
+            endforeach;
+        }
+
+        return array($FormStructure, $FormLabel);
     }
 
 }
