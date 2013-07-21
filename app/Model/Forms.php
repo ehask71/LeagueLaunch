968169c0007c8fe87abd4da0395e28d6a0e13428
Eric Haskins <ehask71@gmail.com>
2013-07-21 03:41:53 -0400

    getFormsById added and Cache Wrapper

--- app/Model/Forms.php
+++ app/Model/Forms.php
@@ -7,5 +7,13 @@ App::uses('AppModel', 'Model');
 
 class Forms extends AppModel {
     public $primaryKey = 'id';
+    
+    public function getFormsById($id){
+        if (($form = Cache::read('getFormById'.$id)) === false) {
+            $form = $this->find('first', array('conditions' => array('Form.id'=>$id,'Form.site_id'=>  Configure::read('Settings.site_id'))));
+            Cache::write('getFormsById'.$id, $form);
+        }
+        return $form;
+    }
 }
 
