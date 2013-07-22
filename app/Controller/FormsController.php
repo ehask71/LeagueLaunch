11b179adccce9fd285bacb29dd3aba8506e59a48
Eric Haskins <ehask71@gmail.com>
2013-07-22 14:46:49 -0400

    Forms

--- app/Controller/FormsController.php
+++ app/Controller/FormsController.php
@@ -39,7 +39,7 @@ class FormsController extends AppController {
 	    if (isset($results['errors']) && !empty($results['errors'])) {
 		$this->Session->setFlash($msg, 'alert/error');
 	    }
-	    $this->preview($id, $results, true);
+	    $this->admin_preview($id, $results, true);
 	}
 	$this->render('preview');
     }
@@ -114,8 +114,66 @@ class FormsController extends AppController {
 	    $this->request->data = $this->Forms->find('first', array('conditions' => array('Forms.id' => $id, 'Forms.site_id' => Configure::read('Settings.site_id'))));
 	}
     }
+    
+    public function preview($id, $results=null, $skipPreview=false){
+	if ($this->RequestHandler->isAjax()) {
+	    $this->autoRender = false;
+	    //preview settings
+	    if (isset($this->request->data['Forms']) && !empty($this->request->data['Forms'])) {
+		$this->Session->write('Preview.Design', $this->request->data['Forms']);
+		return true;
+	    }
+	}
 
-    public function admin_preview($id) {
+	$forms = null;
+	if ($id) {
+	    $forms = $this->Forms->getFormsById($id);
+	}
+	$msg = ($forms['Forms']['error_message']) ? $forms['Forms']['error_message'] : __('Oops...something went wrong when trying to save your Form responses. Take a look at the errors below and try again.', true);
+
+	$this->set(compact('forms', 'id'));
+
+	if ($this->Session->check('Preview') && !$skipPreview) {
+	    $forms['Forms'] = $this->Session->read('Preview.Design');
+	    $this->set(compact('forms'));
+
+	    $this->request->data['Forms'] = $this->Session->read('Preview.Design');
+	}
+
+	if (!empty($this->request->data)) {
+	    $params = null;
+	    if (isset($this->request->data['Forms']['frmb']) && !empty($this->request->data['Forms']['frmb'])) {
+		$params = unserialize($this->request->data['Forms']['frmb']);
+		$form = new Formbuilder($params);
+		$form->setMessage($msg);
+		//$form->setControlPerPage($survey['Forms']['controls_per_page']);
+		$this->set('renderForm', $form->render_html('save_form', $forms, $results));
+	    } else {
+		if (isset($this->request->data['Forms']['design']) && !empty($this->request->data['Forms']['design'])) {
+		    $params = json_decode($this->request->data['Forms']['design'], true);
+		    $form = new Formbuilder($params);
+		    $for_db = $form->get_encoded_form_array();
+		    $form = new Formbuilder($for_db);
+		    $form->setMessage($msg);
+		    //$form->setControlPerPage($forms['Forms']['controls_per_page']);
+		    $form->setPreviewState(true);
+		    $this->set('renderForm', $form->render_html('/surveys/save_response/' . $id, $forms, $results));
+		}
+	    }
+	} else {
+	    if ($forms) {
+		$formStructure = unserialize($forms['Forms']['form_structure']);
+		$form = new Formbuilder($formStructure);
+		$form->setMessage($msg);
+		//$form->setControlPerPage($forms['Forms']['controls_per_page']);
+		$this->set('renderForm', $form->render_html(FULL_BASE_URL . $this->base . '/survey/survey_responses/save_response/' . $id, $forms, $results));
+	    } else {
+		$this->set('renderForm', null);
+	    }
+	}
+    }
+    
+    public function admin_preview($id, $results=null, $skipPreview=false) {
 	if ($this->RequestHandler->isAjax()) {
 	    $this->autoRender = false;
 	    //preview settings
