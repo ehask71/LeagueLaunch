c512d08a63909d0dfc777f3fd6cef7a00678d00e
Eric Haskins <ehask71@gmail.com>
2013-07-22 13:40:03 -0400

    CloudFlare Helper

--- app/Model/Forms.php
+++ app/Model/Forms.php
@@ -9,7 +9,7 @@ class Forms extends AppModel {
     public $primaryKey = 'id';
     
     public function getFormsById($id){
-        if (($form = Cache::read('getFormById'.$id)) === false) {
+        if (($form = Cache::read('getFormsById'.$id)) === false) {
             $form = $this->find('first', array('conditions' => array('Forms.id'=>$id,'Forms.site_id'=>  Configure::read('Settings.site_id'))));
             Cache::write('getFormsById'.$id, $form);
         }
