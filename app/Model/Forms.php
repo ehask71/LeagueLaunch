a0432d96498c74f1b2798a3e5cbdb4e9d62e91c4
Eric Haskins <ehask71@gmail.com>
2013-07-22 15:58:37 -0400

    Forms

--- app/Model/Forms.php
+++ app/Model/Forms.php
@@ -8,6 +8,18 @@ App::uses('AppModel', 'Model');
 class Forms extends AppModel {
     public $primaryKey = 'id';
     
+    public function afterSave($created) {
+        parent::afterSave($created);
+        Cache::clear();
+        clearCache();
+    }
+
+    public function  afterDelete() {
+        parent::afterDelete();
+        Cache::clear();
+        clearCache();
+    }
+    
     public function getFormsById($id){
         if (($form = Cache::read('getFormsById'.$id)) === false) {
             $form = $this->find('first', array('conditions' => array('Forms.id'=>$id,'Forms.site_id'=>  Configure::read('Settings.site_id'))));
