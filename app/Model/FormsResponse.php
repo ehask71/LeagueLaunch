f9e52f3f7a2742bc67b9c14ff8984da6bbe2dafb
Eric Haskins <ehask71@gmail.com>
2013-07-23 22:56:30 -0400

    Form Edit

--- app/Model/FormsResponse.php
+++ app/Model/FormsResponse.php
@@ -7,5 +7,32 @@ App::uses('AppModel', 'Model');
 
 class FormsResponse extends AppModel {
     public $primaryKey = 'id';
+    public $belongsTo = array(
+        'Forms' => array(
+                'className' => 'Forms',
+                'foreignKey' => 'form_id',
+                'fields' => '',
+                'order' => '',
+                'counterCache' => true
+            )
+    );
+    
+    public function afterSave($created) {
+        parent::afterSave($created);
+
+        $id = ($this->getLastInsertID()) ? $this->getLastInsertID() : $this->id;
+        if($id){
+            Cache::delete('getResponseById'.$id);
+        }
+    }
+    
+    public function  afterDelete() {
+        parent::afterDelete();
+
+        $id = (isset($this->data['FormsResponse']['id']) && $this->data['FormsResponse']['id']) ? $this->data['FormsResponse']['id'] : $this->id;
+        if($id){
+            Cache::delete('getResponseById'.$id);
+        }
+    }
 }
 
