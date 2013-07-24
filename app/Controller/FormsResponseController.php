fb792f2abc1316a8880bc0b1c5b9142de4e742dc
Eric Haskins <ehask71@gmail.com>
2013-07-24 01:53:56 -0400

    Form Edit

--- app/Controller/FormsResponseController.php
+++ app/Controller/FormsResponseController.php
@@ -14,6 +14,7 @@ class FormsResponseController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
+        $this->Auth->allow('save_response');
     }
 
     public function index() {
