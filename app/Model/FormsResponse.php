730190184cb8a8734a9411d93a8eb0807d9c6225
Eric Haskins <ehask71@gmail.com>
2013-07-23 23:01:37 -0400

    Form Edit

--- app/Model/FormsResponse.php
+++ app/Model/FormsResponse.php
@@ -34,5 +34,50 @@ class FormsResponse extends AppModel {
             Cache::delete('getResponseById'.$id);
         }
     }
+    
+    public function beforeDelete(){
+        parent::beforeDelete();
+
+        $id = (isset($this->data['FormsResponse']) && $this->data['FormsResponse']['id']) ? $this->data['FormsResponse']['id'] : $this->id;
+
+        return true;
+    }
+
+    /**
+     *  Get list of question to next, prev
+     *
+     * @param <type> $practice_test_id
+     */
+    public function getResponseList($survey_id=null) {
+        $responseList = $this->find('list', array(
+                    'order'=>array('FormsResponse.created'=>'ASC'),
+                    'conditions' => array('FormsResponse.survey_id' => $survey_id,'FormsResponse.site_id' => Configure::read('Settings.site_id'))
+                ));
+        $res = null;
+        $i=0;
+        foreach($responseList as $id){
+            $i++;
+            $res[$id] = __('Response', true).' #'.$i;
+        }
+        return $res;
+    }
+    /**
+     *  Get response by id
+     *
+     * @param <type> $practice_test_id
+     */
+    public function getResponseById($id=null, $form_id=null) {
+
+        $option = array('order'=>array('FormsResponse.created'=>'ASC'), 'fields'=>array('Forms.name', 'Forms.form_structure', 'FormsResponse.*'), 'limit'=>1);
+        if($form_id){
+            $option['conditions'] = array('FormsResponse.form_id' => $form_id);
+        }
+        if($id){
+            $option = array('conditions' => array('FormsResponse.id' => $id), 'fields'=>array('Forms.name', 'Forms.form_structure','FormsResponse.*'));
+        }
+        $response = $this->find('first', $option);
+
+        return $response;
+    }
 }
 
