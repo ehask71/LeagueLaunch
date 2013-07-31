dc8c9c5107eac7647cee2d2f1de8337aa8975dc5
Eric Haskins <ehask71@gmail.com>
2013-07-30 22:34:04 -0400

    getProductById  added

--- app/Model/Products.php
+++ app/Model/Products.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP Products
  * @author Eric
@@ -6,14 +7,26 @@
 App::uses('AppModel', 'Model');
 
 class Products extends AppModel {
+
     public $primaryKey = 'id';
-    
-    public function getProductById($id){
-        if (($product = Cache::read('getProductById'.$id)) === false) {
-            $product = $this->find('first', array('conditions' => array('Products.id'=>$id,'Products.site_id'=>  Configure::read('Settings.site_id'),'Products.active'=>1)));
-            Cache::write('getProductById'.$id, $product);
+
+    public function getProductById($id) {
+        if (($product = Cache::read('getProductById' . $id)) === false) {
+            $product = $this->find('first', array('conditions' => array('Products.id' => $id, 'Products.site_id' => Configure::read('Settings.site_id'), 'Products.active' => 1)));
+            Cache::write('getProductById' . $id, $product);
         }
         return $product;
     }
+
+    public function getRegistrationProducts() {
+        return $this->find('all', array(
+                    'order' => 'Products.id DESC',
+                    'conditions' => array(
+                        'Products.site_id' => Configure::read('Settings.site_id'),
+                        'Products.active' => 1,
+                        'Products.category_id' => 1 
+                        )));
+    }
+
 }
 
