c2fb9d3e8f06c6fbc181477e054168dd6c920dac
Eric Haskins <ehask71@gmail.com>
2013-07-30 20:13:56 -0400

    Form Edit

--- app/Controller/ContactController.php
+++ app/Controller/ContactController.php
@@ -4,9 +4,11 @@
  * @author Eric
  */
 App::uses('AppController', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
 
 class ContactController extends AppController {
-
+    public $name = 'Contact';
+    
     public function index(){
         $this->set('title_for_layout','Contact Us');
     }
