8587db2b86ff6b48b9ecdd0fc304445203148974
Eric Haskins <ehask71@gmail.com>
2013-07-30 20:35:09 -0400

    getProductById  added

--- app/Model/Products.php
+++ app/Model/Products.php
@@ -7,5 +7,13 @@ App::uses('AppModel', 'Model');
 
 class Products extends AppModel {
     public $primaryKey = 'id';
+    
+    public function getProductById($id){
+        if (($product = Cache::read('getProductById'.$id)) === false) {
+            $product = $this->find('first', array('conditions' => array('Products.id'=>$id,'Products.site_id'=>  Configure::read('Settings.site_id'),'Products.active'=>1)));
+            Cache::write('getProductById'.$id, $product);
+        }
+        return $product;
+    }
 }
 
