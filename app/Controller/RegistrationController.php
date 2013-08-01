5e1eecdfa3598b353f516bd7d564d130dd727407
Eric Haskins <ehask71@gmail.com>
2013-08-01 00:54:50 -0400

    step1 action code added

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -10,7 +10,7 @@ class RegistrationController extends AppController {
     
     public $name = 'Registration';
     
-    public $uses = array('Products','Forms');
+    public $uses = array('Products','Forms','Players');
     
     
     public function beforeFilter() {
@@ -23,9 +23,11 @@ class RegistrationController extends AppController {
     }
     
     public function step1(){
-        $registrations = $this->Products->getRegistrationProducts();
-	
-	$this->set(compact('registrations'));
+        $user = $this->Auth->user();
+        $registration_options = $this->Products->getRegistrationDropDown();
+	$players = $this->Players->getPlayersByUser($user['id'], Configure::read('Settings.site_id'));
+	$this->set(compact('registration_options'));
+        $this->set(compact('players'));
     }
     /*
      *    Show Registrations Available
