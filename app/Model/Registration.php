40ca0bbe420ad17539a0410e9ac6f96e5a091f60
Eric Haskins <ehask71@gmail.com>
2013-08-01 16:20:18 -0400

    Alerts flash.js

---
+++ app/Model/Registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CakePHP Registration
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class Registration extends AppModel {
+    
+    public $primaryKey = 'id';
+    
+    
+}
+
