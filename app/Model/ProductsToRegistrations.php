bfb8c4c0d6fa1cf93d9487b8b24cfd32adf2724b
Eric Haskins <ehask71@gmail.com>
2013-08-01 22:08:53 -0400

    regi

---
+++ app/Model/ProductsToRegistrations.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * CakePHP ProductsToRegistrations
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class ProductsToRegistrations extends AppModel {
+    public $primaryKey = 'id';
+    
+    public $useTable = 'products_to_registrations';
+    
+    
+}
+
