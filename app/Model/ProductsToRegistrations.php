6f4ec1d4e92cc04e28d8f599ed2fb98d938e594d
Eric Haskins <ehask71@gmail.com>
2013-08-02 02:30:02 -0400

    regi

--- app/Model/ProductsToRegistrations.php
+++ app/Model/ProductsToRegistrations.php
@@ -13,72 +13,67 @@ class ProductsToRegistrations extends AppModel {
 
     public function getRegistrationsDropdown($regs) {
         $opts = array();
-        if (count($regs) > 0) {
-            foreach ($regs AS $row) {
-                $products = $this->find('all', array(
+        $products = $this->find('all', array(
+            'conditions' => array(
+                'ProductsToRegistrations.regid' => $regs,
+                'ProductsToRegistrations.site_id' => Configure::read('Settings.site_id'),
+                'Products.active' => 1,
+                'Products.category_id' => 1
+            ),
+            'joins' => array(
+                array(
+                    'table' => 'products',
+                    'alias' => 'Products',
+                    'type' => 'INNER',
                     'conditions' => array(
-                        'ProductsToRegistrations.regid' => $row['Registration']['id'],
-                        'ProductsToRegistrations.site_id' => $row['Registration']['site_id'],
-                        'Products.active' => 1,
-                        'Products.category_id' => 1
-                    ),
-                    'joins' => array(
-                        array(
-                            'table' => 'products',
-                            'alias' => 'Products',
-                            'type' => 'INNER',
-                            'conditions' => array(
-                                'ProductsToRegistrations.product_id = Products.id'
-                            )
-                        )
-                    ),
-                    'fields' => array('Products.*','ProductsToRegistrations.*')
-                        ));
-                if(count($products) > 0){
-                    foreach ($products AS $prod){
-                        $opts[$prod['Products']['id']] = $prod['Products']['name'] . ' ($' . $prod['Products']['price'] . ')';
-                    }
-                }
+                        'ProductsToRegistrations.product_id = Products.id'
+                    )
+                )
+            ),
+            'fields' => array('Products.*', 'ProductsToRegistrations.*')
+                ));
+        if (count($products) > 0) {
+            foreach ($products AS $prod) {
+                $opts[$prod['Products']['id']] = $prod['Products']['name'] . ' ($' . $prod['Products']['price'] . ')';
             }
-            if(count($opts)>0){
-                return $opts;
-            }
+        }
+
+        if (count($opts) > 0) {
+            return $opts;
         }
         return false;
     }
-    
-    public function getUpSells($regs){
+
+    public function getUpSells($regs) {
         $opts = array();
-        if (count($regs) > 0) {
-            foreach ($regs AS $row) {
-                $products = $this->find('all', array(
+
+        $products = $this->find('all', array(
+            'conditions' => array(
+                'ProductsToRegistrations.regid' => $regs,
+                'ProductsToRegistrations.site_id' => Configure::read('Settings.site_id'),
+                'Products.active' => 1,
+                'Products.category_id' => 2
+            ),
+            'joins' => array(
+                array(
+                    'table' => 'products',
+                    'alias' => 'Products',
+                    'type' => 'INNER',
                     'conditions' => array(
-                        'ProductsToRegistrations.regid' => $row['Registration']['id'],
-                        'ProductsToRegistrations.site_id' => $row['Registration']['site_id'],
-                        'Products.active' => 1,
-                        'Products.category_id' => 2
-                    ),
-                    'joins' => array(
-                        array(
-                            'table' => 'products',
-                            'alias' => 'Products',
-                            'type' => 'INNER',
-                            'conditions' => array(
-                                'ProductsToRegistrations.product_id = Products.id'
-                            )
-                        )
-                    ),
-                    'fields' => array('Products.*','ProductsToRegistrations.*')
-                        ));
-                if(count($products) > 0){
-                    foreach ($products AS $prod){
-                        $opts[$prod['Products']['id']] = $prod['Products'];
-                    }
-                }
+                        'ProductsToRegistrations.product_id = Products.id'
+                    )
+                )
+            ),
+            'fields' => array('Products.*', 'ProductsToRegistrations.*')
+                ));
+        if (count($products) > 0) {
+            foreach ($products AS $prod) {
+                $opts[$prod['Products']['id']] = $prod['Products'];
             }
-            if(count($opts)>0){
-                return $opts;
-            }
+        }
+
+        if (count($opts) > 0) {
+            return $opts;
         }
         return FALSE;
     }
