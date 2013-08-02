37157d50c00a345cde4acd7dab0ea678984d0913
Eric Haskins <ehask71@gmail.com>
2013-08-01 20:51:37 -0400

    regi

--- app/Model/Registration.php
+++ app/Model/Registration.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP Registration
  * @author Eric
@@ -6,9 +7,31 @@
 App::uses('AppModel', 'Model');
 
 class Registration extends AppModel {
-    
+
     public $primaryKey = 'id';
-    
-    
+
+    public function validateRegistration() {
+        $validate1 = array(
+            'name' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a Name')
+            ),
+            'startdate' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a Start Date')
+            ),
+            'enddate' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a End Date')
+            )
+        );
+
+        $this->validate = $validate1;
+        return $this->validates();
+    }
+
 }
 
