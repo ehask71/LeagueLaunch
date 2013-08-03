b22b6db38632266e95189b240f929740c2033509
Eric Haskins <ehask71@gmail.com>
2013-08-03 14:20:08 -0400

    paypal pay now button

--- app/Model/PlayersToSeasons.php
+++ app/Model/PlayersToSeasons.php
@@ -15,11 +15,16 @@ class PlayersToSeasons extends AppModel {
         $data['PlayersToSeasons']['regid'] = (int)$regid;
         $data['PlayersToSeasons']['site_id'] = Configure::read('Settings.site_id');
         $data['PlayersToSeasons']['player_id'] = (int)$player;
+        $data['PlayersToSeasons']['product_id'] = $product_id;
         $data['PlayersToSeasons']['haspaid'] = (isset($opts['haspaid']))?$opts['haspaid']:0;
         $data['PlayersToSeasons']['formcomplete'] = (isset($opts['haspaid']))?$opts['haspaid']:0;
         $data['PlayersToSeasons']['verifydocs'] = (isset($opts['haspaid']))?$opts['haspaid']:0;
         
-        $this->save($data);
+        if($this->save($data)){
+            return $this->getLastInsertID();
+        }
+        
+        return false;
     }
 }
 
