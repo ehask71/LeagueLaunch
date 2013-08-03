8c10e12bca4686209855f6c2a488e432d4d482d5
Eric Haskins <ehask71@gmail.com>
2013-08-03 12:09:28 -0400

    paypal pay now button

---
+++ app/Model/Season.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CakePHP Season
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class Season extends AppModel {
+    
+    public $name = 'Season';
+    public $primaryKey = 'id';
+    
+}
+
