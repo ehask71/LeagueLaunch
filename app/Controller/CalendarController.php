60fb4ee007215ad84e349f01f8c9ffd959d8c424
Eric Haskins <ehask71@gmail.com>
2013-08-03 15:44:40 -0400

    paypal pay now button

--- app/Controller/CalendarController.php
+++ app/Controller/CalendarController.php
@@ -10,6 +10,7 @@ class CalendarController extends AppController {
         
     public function beforeFilter() {
 	parent::beforeFilter();
+        $this->Auth->allow('index');
     }
     
     public function index(){
