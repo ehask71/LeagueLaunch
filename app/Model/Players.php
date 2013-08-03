8c70d1a339dfc9e72a207f5248a97e80e1472124
Eric Haskins <ehask71@gmail.com>
2013-08-03 15:38:28 -0400

    paypal pay now button

--- app/Model/Players.php
+++ app/Model/Players.php
@@ -1,29 +1,43 @@
 <?php
+
 App::uses('AppModel', 'Model');
 
 class Players extends AppModel {
+
     public $primaryKey = 'player_id';
-    
-    public function getPlayersByUser($id,$site_id=false){
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'counterCache' => true,
+            'counterScope' => array(),
+        )
+    );
+
+    public function getPlayersByUser($id, $site_id = false) {
         $conditions['Players.active'] = 1;
-        $conditions['Players.user_id'] = (int)$id;
-        if($site_id){
-            $conditions['Players.site_id'] = (int)$site_id;
+        $conditions['Players.user_id'] = (int) $id;
+        if ($site_id) {
+            $conditions['Players.site_id'] = (int) $site_id;
         }
-        
+
         return $this->find('all', array(
-		    'order' => 'Players.player_id DESC',
-		    'conditions' => $conditions 
-            ));
+                    'order' => 'Players.player_id DESC',
+                    'conditions' => $conditions
+                ));
     }
-    
-    public function getPlayerById($id,$site_id=FALSE){
-	return $this->find('first',array(
-	    'conditions' => array(
-		'Players.player_id' => (int)$id
-	    )
-	));
+
+    public function getPlayerById($id, $site_id = FALSE) {
+        return $this->find('first', array(
+                    'conditions' => array(
+                        'Players.player_id' => (int) $id
+                    )
+                ));
     }
+
 }
 
 ?>
