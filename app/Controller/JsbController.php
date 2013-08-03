1dc48194814adf4a520c7802661134d1b657300d
Eric Haskins <ehask71@gmail.com>
2013-08-03 00:10:00 -0400

    regi

---
+++ app/Controller/JsbController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * CakePHP JsController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class JsbController extends AppController {
+    
+    public $name = 'Jsb';
+    
+    public function beforeFilter() {
+	parent::beforeFilter();
+        $this->Auth->allow('index');
+    }
+    
+    public function index(){
+        $this->autoRender = false;
+        echo '<html><head>
+            <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+            </head><body>';
+        echo '<script type="text/javascript">';
+	echo '$(parent.document).find(\'.ui-dialog\');';
+	echo 'window.parent.$(\'#dialogIDname\').dialog(\'close\');'; 
+        echo '</script></body></html>';
+    }
+    
+}
+
