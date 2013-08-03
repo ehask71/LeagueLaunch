ae1a203b08aabb5add4a5519db76bd4fb68dd987
Eric Haskins <ehask71@gmail.com>
2013-08-03 15:44:15 -0400

    paypal pay now button

--- app/Controller/SponsorsController.php
+++ app/Controller/SponsorsController.php
@@ -11,6 +11,7 @@ class SponsorsController extends AppController {
     
     public function beforeFilter() {
 	parent::beforeFilter();
+        $this->Auth->allow('index');
     }
     
     public function index(){
