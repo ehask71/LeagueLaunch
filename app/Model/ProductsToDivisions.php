078d50e8811739f7de8662732036ee6765a87e16
Eric Haskins <ehask71@gmail.com>
2013-08-04 02:58:25 -0400

    Season model Updated validate

---
+++ app/Model/ProductsToDivisions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * CakePHP ProductsToDivisions
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class ProductsToDivisions extends AppModel {
+    public $primaryKey = 'ProductsToDivisions_id';
+}
+
