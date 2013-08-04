ab26137abe158270421368cd14c203f6044cccec
Eric Haskins <ehask71@gmail.com>
2013-08-04 04:56:46 -0400

    Season model Updated validate

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -262,7 +262,6 @@ class RegistrationController extends AppController {
                     $shop['Order']['order_id'] = $orderid;
                     $this->Session->write('Shop.Order.order_id', $orderid);
                     $shop['Order']['season_id'] = $this->Session->read('Season.id');
-                    $this->Session->write('Shop.Order.regid', $shop['Order']['regid']);
                     $this->Session->write('Shop.Order.season_id', $shop['Order']['season_id']);
 
                     // Do the insert for Player_to_Registrations
