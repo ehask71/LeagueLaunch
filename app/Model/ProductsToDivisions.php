af08941902ca0293055a30087786de46d6bd39a0
Eric Haskins <ehask71@gmail.com>
2013-08-04 02:59:39 -0400

    Season model Updated validate

--- app/Model/ProductsToDivisions.php
+++ app/Model/ProductsToDivisions.php
@@ -6,6 +6,10 @@
 App::uses('AppModel', 'Model');
 
 class ProductsToDivisions extends AppModel {
-    public $primaryKey = 'ProductsToDivisions_id';
+    
+    public $primaryKey = 'id';
+    public $useTable = 'products_to_divisions';
+    
+    
 }
 
