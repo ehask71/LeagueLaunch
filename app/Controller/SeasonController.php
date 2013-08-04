59c19f29ab6570ff9f4bb604468a006224eb34d8
Eric Haskins <ehask71@gmail.com>
2013-08-04 01:29:52 -0400

    Season model Updated validate

--- app/Controller/SeasonController.php
+++ app/Controller/SeasonController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP SeasonsController
  * @author Eric
@@ -6,33 +7,43 @@
 App::uses('AppController', 'Controller');
 
 class SeasonController extends AppController {
-    
+
     public $name = 'Season';
     public $uses = array('Season');
-    
+
     public function beforeFilter() {
-	parent::beforeFilter();
+        parent::beforeFilter();
     }
-    
-    public function index(){
+
+    public function index() {
         
     }
-    
-    public function admin_index(){
+
+    public function admin_index() {
         if ($this->request->is('post')) {
-	    if ($this->Divisions->divisionValidate()) {
-		$this->Divisions->save($this->request->data, false);
-		$this->Session->setFlash(__('The Division was Added!'),'default',array('class'=>'alert succes_msg'));
-		$this->redirect('/admin/divisions');
-	    }
-	}
-	$this->paginate = array(
-	    'conditions' => array(
-		'Divisions.site_id' => Configure::read('Settings.site_id')
-	    ) 
-	);
-	$divisions = $this->paginate('Divisions');
-	$this->set(compact('divisions'));
+            if ($this->Season->seasonValidate()) {
+                $this->Season->save($this->request->data, false);
+                $this->Season->setFlash(__('The Season was Added!'), 'default', array('class' => 'alert succes_msg'));
+                $this->redirect('/admin/season');
+            }
+        }
+        $this->paginate = array(
+            'conditions' => array(
+                'Season.site_id' => Configure::read('Settings.site_id')
+            )
+        );
+        $seasons = $this->paginate('Season');
+        $this->set(compact('seasons'));
     }
+
+    public function admin_new() {
+        if ($this->Season->seasonValidate()) {
+            $this->Season->save($this->request->data, false);
+            $this->Season->setFlash(__('The Season was Added!'), 'default', array('class' => 'alert succes_msg'));
+            $this->redirect('/admin/season');
+        }
+        
+    }
+
 }
 
