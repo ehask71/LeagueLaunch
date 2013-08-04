6b3507ede178dcb32bd8e46fd52cd1651c4325f1
Eric Haskins <ehask71@gmail.com>
2013-08-03 21:55:24 -0400

    paypal pay now button

--- app/Controller/ContactController.php
+++ app/Controller/ContactController.php
@@ -9,6 +9,11 @@ App::uses('CakeEmail', 'Network/Email');
 class ContactController extends AppController {
     public $name = 'Contact';
     
+    public function beforeFilter() {
+	parent::beforeFilter();
+        $this->Auth->allow('index');
+    }
+    
     public function index(){
         
         $this->set('title_for_layout','Contact Us');
