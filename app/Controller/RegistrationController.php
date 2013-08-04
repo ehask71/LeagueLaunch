692f497ab75d702fb1df3b57e865cf862c2b9a93
Eric Haskins <ehask71@gmail.com>
2013-08-04 04:21:13 -0400

    Season model Updated validate

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -162,8 +162,6 @@ class RegistrationController extends AppController {
             if (count($this->request->data['Players']) > 0) {
                 foreach ($this->request->data['Players'] AS $k => $v) {
                     $product = $this->Products->getProductsByDivision($v,$this->Session->read('Season.id'));
-                    print_r($product);
-                    print $this->Session->read('Season.id');
                     $this->Cart->add($product['Products']['id'], 1);
                     $player = $this->Players->getPlayerById($k);
                     $this->Session->write('Player.' . $k . '.product', $product['Products']['id']);
