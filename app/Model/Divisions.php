9da4483fb99304821867e4d8bd6fc665940e67ea
Eric Haskins <ehask71@gmail.com>
2013-08-03 22:21:02 -0400

    paypal pay now button

--- app/Model/Divisions.php
+++ app/Model/Divisions.php
@@ -24,7 +24,7 @@ class Divisions extends AppModel {
             'conditions' => array('Divisions.site_id' => Configure::read('Settings.site_id'))));
         $opts = array();
         if(count($div)>0){
-            $opts[NULL] = 'Choose Parent Division';
+            $opts[0] = 'Choose Parent Division';
             foreach($div AS $row){
                 $opts[$row['Divisions']['division_id']] =  $row['Divisions']['name'];
             }
