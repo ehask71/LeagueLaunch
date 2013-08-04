967713967aab0ba51e61f2c712d4fbcf13cf9e00
Eric Haskins <ehask71@gmail.com>
2013-08-03 22:04:50 -0400

    paypal pay now button

--- app/Model/Divisions.php
+++ app/Model/Divisions.php
@@ -7,16 +7,30 @@ class Divisions extends AppModel {
     public $primaryKey = 'division_id';
 
     public function divisionValidate() {
-	$validate1 = array(
-	    'leaguename' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please enter the League\'s Name')
-	    ),
-	);
-	
-	$this->validate = $validate1;
+        $validate1 = array(
+            'leaguename' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the League\'s Name')
+            ),
+        );
+
+        $this->validate = $validate1;
         return $this->validates();
+    }
+
+    public function getDivisionsDropdown() {
+        $div = $this->find('all', array(
+            'conditions' => array('Divisions.site_id' => Configure::read('Settings.site_id'))));
+        $opts = array();
+        if(count($div)>0){
+            $opts[NULL] = 'Choose Parent Division';
+            foreach($div AS $row){
+                $opts[$row['Divisions']['division_id']] =  $row['Divisions']['name'];
+            }
+        }
+        
+        return $opts;
     }
 
 }
