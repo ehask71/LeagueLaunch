923dc6401cfafef06347b743111339dfa31b2e82
Eric Haskins <ehask71@gmail.com>
2013-08-05 15:57:17 -0400

    playball banner and webroot/img/baseball/ folder

--- app/Controller/Component/CartComponent.php
+++ app/Controller/Component/CartComponent.php
@@ -73,7 +73,7 @@ class CartComponent extends Component {
 		$data['subtotal'] = sprintf('%01.2f', $product['Products']['price'] * $quantity);
 		$data['totalweight'] = sprintf('%01.2f', $product['Products']['weight'] * $quantity);
 		$data['Product'] = $product['Products'];
-                $data['player_id'] = (int)($player)?$players:0;
+                $data['player_id'] = (int)($player)?$player:0;
                 $data['season_id'] = (int)($season)?$season:0;
 		$this->Session->write('Shop.OrderItem.' . $id, $data);
 		$this->Session->write('Shop.Order.shop', 1);
