26c281342ae17a4da0a0b44c44cd4f8c7fb8a509
Eric Haskins <ehask71@gmail.com>
2013-08-06 13:32:56 -0400

    form-text added

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -4,19 +4,6 @@ App::uses('Model', 'Model');
 
 class AppModel extends Model {
 
-    // For custom find('xxx') methods. The function looks for a __findFindType class method.
-    function find($type, $options = array()) {
-	$method = null;
-	if (is_string($type)) {
-	    $method = sprintf('__find%s', Inflector::camelize($type));
-	}
-
-	if ($method && method_exists($this, $method)) {
-	    return $this->{$method}($options);
-	} else {
-	    $args = func_get_args();
-	    return call_user_func_array(array('parent', 'find'), $args);
-	}
-    }
+   
 
 }
