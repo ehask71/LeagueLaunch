404915f3199ee3d059607cc3dc39c0070190468e
Eric Haskins <ehask71@gmail.com>
2013-08-13 15:56:52 -0400

    Season view

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -289,6 +289,7 @@ class RegistrationController extends AppController {
                 if ($save) {
                     $orderid = $this->Order->getLastInsertID();
                     $shop['Order']['order_id'] = $orderid;
+		    $shop['Order']['user_id'] = $this->Auth->user('id');
                     $this->Session->write('Shop.Order.order_id', $orderid);
                     $shop['Order']['season_id'] = $this->Session->read('Season.id');
                     $this->Session->write('Shop.Order.season_id', $shop['Order']['season_id']);
