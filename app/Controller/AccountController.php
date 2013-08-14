ee1ae8415f409da6b57ae725116bfc4eda0e4f94
Eric Haskins <ehask71@gmail.com>
2013-08-14 12:04:23 -0400

    forgetpwd

--- app/Controller/AccountController.php
+++ app/Controller/AccountController.php
@@ -8,12 +8,12 @@ App::uses('AppController', 'Controller');
 
 class AccountController extends AppController {
 
-    public $uses = array('Account', 'RoleUser', 'Country','Order');
+    public $uses = array('Account', 'RoleUser', 'Country', 'Order');
     public $components = array('Email');
 
     public function beforeFilter() {
 	parent::beforeFilter();
-	$this->Auth->allow('login', 'logout', 'register', 'forgetpwd', 'confirmpwd','resetcode','entercode');
+	$this->Auth->allow('login', 'logout', 'register', 'forgetpwd', 'confirmpwd', 'resetcode', 'entercode');
     }
 
     public function isAuthorized($user) {
@@ -27,17 +27,17 @@ class AccountController extends AppController {
 	}
 	return true;
     }
-    
-    public function index(){
-        $account = $this->Account->find('first',array(
-           'conditions' => array(
-               'Account.id' => $this->Auth->user('id')
-           ) 
-        ));
-        
-        $this->set(compact('account'));
+
+    public function index() {
+	$account = $this->Account->find('first', array(
+	    'conditions' => array(
+		'Account.id' => $this->Auth->user('id')
+	    )
+		));
+
+	$this->set(compact('account'));
     }
-    
+
     public function login() {
 	if ($this->request->is('post')) {
 	    if ($this->Auth->login()) {
@@ -70,43 +70,106 @@ class AccountController extends AppController {
 
     public function forgetpwd() {
 	if ($this->request->is('post')) {
-	    $account = $this->Account->find('first',array(
+	    $account = $this->Account->find('first', array(
 		'conditions' => array(
 		    'Account.email' => $this->request->data['Account']['email']
-		)   
-	    ));
-	    if(count($account)> 0){
+		)
+		    ));
+	    if (count($account) > 0) {
 		$data['id'] = $account['Account']['id'];
-		$data['reset_code'] = md5($this->request->data['Account']['email'].date('Y-m-d h:i:s'));
-		
-		if($this->Account->save($data)){
+		$data['reset_code'] = md5($this->request->data['Account']['email'] . date('Y-m-d h:i:s'));
+
+		if ($this->Account->save($data)) {
 		    App::uses('CakeEmail', 'Network/Email');
 		    $email = new CakeEmail();
 		    $email->from(array('do-not-reply@leaguelaunch.com' => Configure::read('Settings.leaguename')))
-			->sender(Configure::read('Settings.admin_email'))
-			->replyTo(Configure::read('Settings.admin_email'))
-			->cc(Configure::read('Settings.admin_email'))
-			->to($account['Account']['email'])
-			->subject(Configure::read('Settings.leaguename') . ' Password Reset')
-			->template('forgot_passwd')
-			->theme(Configure::read('Settings.theme'))
-			->emailFormat('text')
-			->viewVars(array('account' => $account,'code' => $data['reset_code']))
-			->send();
+			    ->sender(Configure::read('Settings.admin_email'))
+			    ->replyTo(Configure::read('Settings.admin_email'))
+			    ->cc(Configure::read('Settings.admin_email'))
+			    ->to($account['Account']['email'])
+			    ->subject(Configure::read('Settings.leaguename') . ' Password Reset')
+			    ->template('forgot_passwd')
+			    ->theme(Configure::read('Settings.theme'))
+			    ->emailFormat('text')
+			    ->viewVars(array('account' => $account, 'code' => $data['reset_code']))
+			    ->send();
 		}
-		
 	    }
 	}
     }
-    
-    public function entercode(){
+
+    public function entercode() {
 	print_r($this->request->data);
     }
-    
-    public function resetcode(){
-	print_r($this->request->query['code']);
+
+    public function resetcode() {
+	$this->autoRender = false;
+	$account = array();
+	// From Form
+	if ($this->request->is('post')) {
+	    if ($this->request->data['code'] != '' && strlen($this->request->data['code']) == 32) {
+		$account = $this->Account->find('first', array(
+		    'conditions' => array(
+			'Account.reset_code' => $this->request->data['code'],
+			'Account.site_id' => Configure::read('Setting.site_id')
+		    )
+			));
+	    }
+	    if(isset($this->request->data['password']) && isset($this->request->data['confirm_password']) && isset($this->request->data['rstcode'])){
+		// We are restting the passwd
+		if($this->request->data['password'] == $this->request->data['confirm_password'] && $this->request->data['password'] != ''){
+		    $account = $this->Account->find('first', array(
+		    'conditions' => array(
+			'Account.reset_code' => $this->request->data['rstcode'],
+			'Account.site_id' => Configure::read('Setting.site_id')
+		    )
+			));
+		    
+		    $data['id'] = $account['Account']['id'];
+		    $data['password'] = $this->request->data['password'];
+		    
+		    if($this->Account->save($data)){
+			App::uses('CakeEmail', 'Network/Email');
+			$email = new CakeEmail();
+			$email->from(array('do-not-reply@leaguelaunch.com' => Configure::read('Settings.leaguename')))
+			    ->sender(Configure::read('Settings.admin_email'))
+			    ->replyTo(Configure::read('Settings.admin_email'))
+			    ->cc(Configure::read('Settings.admin_email'))
+			    ->to($account['Account']['email'])
+			    ->subject(Configure::read('Settings.leaguename') . ' Password Changed')
+			    ->template('passwd_changed')
+			    ->theme(Configure::read('Settings.theme'))
+			    ->emailFormat('text')
+			    ->viewVars(array('account' => $account))
+			    ->send();
+			
+			$this->Session->setFlash(__('Password Changed!'),'alerts/success');
+			$this->redirect('/login');
+		    }
+		    
+		} else {
+		    $this->Session->setFlash('Passwords Do Not Match or Blank', 'alerts/error');
+		    $this->redirect('/account/resetcode/?code='.$this->request->data['rstcode']);
+		}
+	    }
+	}
+	// From Email Link
+	if ($this->request->query['code'] != '' && strlen($this->request->query['code']) == 32) {
+	    $account = $this->Account->find('first', array(
+		'conditions' => array(
+		    'Account.reset_code' => $this->request->query['code'],
+		    'Account.site_id' => Configure::read('Setting.site_id')
+		)
+		    ));
+	}
+	
+	if(count($account)){
+	    $this->render('newpassword');
+	} else {
+	    $this->render('entercode');
+	}
     }
-    
+
     public function confirmpwd() {
 	
     }
@@ -115,34 +178,33 @@ class AccountController extends AppController {
 	$this->redirect($this->Auth->logout());
     }
 
-    public function history(){
+    public function history() {
 	
-    }
-    
-    public function orders(){
-	$this->loadModel('Order');
-	$orders = $this->Order->find('all',array(
-	   'conditions' => array(
-	       'Order.user_id' => $this->Auth->user('id'),
-	       'Order.site_id' => Configure::read('Settings.site_id')
-	   )
-	));
-	
-	$this->set(compact('orders'));
-    }
-    
-    public function vieworder($id){
-        $order = $this->Order->find('first',array(
-           'conditions' => array(
-               'Order.id' => $id,
-               'Order.user_id' => $this->Auth->user('id'),
-	       'Order.site_id' => Configure::read('Settings.site_id')
-           ) 
-        ));
-	$this->set('rtn',base64_encode('http://eastbaylittleleague.com/account/vieworder/'.$id));
-        $this->set(compact('order'));
     }
 
+    public function orders() {
+	$this->loadModel('Order');
+	$orders = $this->Order->find('all', array(
+	    'conditions' => array(
+		'Order.user_id' => $this->Auth->user('id'),
+		'Order.site_id' => Configure::read('Settings.site_id')
+	    )
+		));
+
+	$this->set(compact('orders'));
+    }
+
+    public function vieworder($id) {
+	$order = $this->Order->find('first', array(
+	    'conditions' => array(
+		'Order.id' => $id,
+		'Order.user_id' => $this->Auth->user('id'),
+		'Order.site_id' => Configure::read('Settings.site_id')
+	    )
+		));
+	$this->set('rtn', base64_encode('http://eastbaylittleleague.com/account/vieworder/' . $id));
+	$this->set(compact('order'));
+    }
 
     public function admin_index() {
 	$joins = array(
@@ -180,8 +242,8 @@ class AccountController extends AppController {
 			'RolesUser.site_id = ' . Configure::read('Settings.site_id')
 		    )
 	    ))));
-	
-	if(count($user)>0){
+
+	if (count($user) > 0) {
 	    $this->set(compact('user'));
 	}
     }
