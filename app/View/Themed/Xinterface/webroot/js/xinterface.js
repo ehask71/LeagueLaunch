8fde0012f33856770f840ce65d6ae5104b5080f3
Eric Haskins <ehask71@gmail.com>
2013-08-17 19:45:46 -0400

    minYear set to 1950

--- app/View/Themed/Xinterface/webroot/js/xinterface.js
+++ app/View/Themed/Xinterface/webroot/js/xinterface.js
@@ -2,7 +2,7 @@
 var query = /key=([A-Z]*)(&value=([a-z|A-Z]*))?/.exec(location.search),
     key   = query[1],
     value = escape(query[3]);
-
+    alert(query);
 // check if the url is valid
 if (query && key) {
     // call the public function
