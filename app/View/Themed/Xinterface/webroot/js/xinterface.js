ad212a204ac051f129fb43d52d068c6584140e2a
Eric Haskins <ehask71@gmail.com>
2013-08-17 19:46:57 -0400

    minYear set to 1950

--- app/View/Themed/Xinterface/webroot/js/xinterface.js
+++ app/View/Themed/Xinterface/webroot/js/xinterface.js
@@ -1,5 +1,5 @@
 // use a strict regular expression for security
-var query = /key=([A-Z]*)(&value=([a-z|A-Z]*))?/.exec(location.search),
+var query = /key=([a-z|A-Z]*)(&value=([a-z|A-Z]*))?/.exec(location.search),
     key   = query[1],
     value = escape(query[3]);
     alert(query);
