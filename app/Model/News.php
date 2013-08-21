75b954bad2b3be314477fdfde6b2738eb3669375
Eric Haskins <ehask71@gmail.com>
2013-08-20 22:21:41 -0400

    bPopup

--- app/Model/News.php
+++ app/Model/News.php
@@ -10,7 +10,7 @@ class News extends AppModel {
 
     public $primaryKey = 'id';
     
-    public $actsAs = array('Media.Medias');
+    public $actsAs = array('Media.Media');
     
     public function newsValidate() {
 	$validate1 = array(
