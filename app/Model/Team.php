ce0ed702470ea9fc67a6d898749b3a11c57b2dd4
ehask71 <ehask71@gmail.com>
2013-08-25 21:15:16 -0400

    Team Controller

--- app/Model/Team.php
+++ app/Model/Team.php
@@ -10,6 +10,25 @@ class Team extends AppModel {
     public $name = 'Team';
     public $primaryKey = 'team_id';
     
+    public function teamValidate(){
+	
+	$validate1 = array(
+	    'name' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the Team name')
+            ),
+	    'division_id' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select a Division')
+            )
+	);
+	
+	$this->validate = $validate1;
+        return $this->validates();
+    }
+    
     public function getSiteTeams(){
 	
     }
