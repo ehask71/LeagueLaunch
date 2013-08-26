43632485fe1033447e2bc8c075804c8db04a5259
ehask71 <ehask71@gmail.com>
2013-08-26 01:44:15 -0400

    Teams

--- app/Controller/TeamController.php
+++ app/Controller/TeamController.php
@@ -22,7 +22,12 @@ class TeamController extends AppController {
     
     public function admin_index(){
 	if ($this->request->is('post') || $this->request->is('put')) {
-	    
+	    if($this->Team->teamValidate()){
+		if($this->Team->save($this->request->data)){
+		    $this->Session->setFlash(__('Team Added!'),'default',array('class'=>'alert succes_msg'));
+		    $this->redirect('/admin/team/');
+		}
+	    }
 	}
 	$teams = $this->Team->find('all',array(
 	    'conditions' => array(
