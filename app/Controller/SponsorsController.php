26623ded445aa4b5524e797ee4ae92f84029b30b
Eric Haskins <ehask71@gmail.com>
2013-08-29 16:21:34 -0400

    forgetpwd

--- app/Controller/SponsorsController.php
+++ app/Controller/SponsorsController.php
@@ -8,6 +8,7 @@ App::uses('AppController', 'Controller');
 class SponsorsController extends AppController {
     
     public $name = 'Sponsors';
+    public $uses = array('Products');
     
     public function beforeFilter() {
 	parent::beforeFilter();
@@ -18,8 +19,26 @@ class SponsorsController extends AppController {
         
     }
     
-    public function options(){
+    public function puchase(){
+	$plans = $this->Products->find('all',array(
+	    'conditions' => array(
+		'Products.site_id' => Configure::read('Settings.site_id'),
+		'Products.category_id' => 3,
+		'Products.active' => 1
+	    )
+	));
 	
+	$this->set(compact('plans'));
+    }
+    
+    public function review(){
+	$shop = $this->Session->read('Shop');
+	if (!$shop['Order']['total']) {
+            $this->Session->setFlash(__('No Items Selected!', 'alerts/error'));
+            $this->redirect('/sponsors/plans');
+        }
+	
+	$this->set(compact('shop'));
     }
 }
 
