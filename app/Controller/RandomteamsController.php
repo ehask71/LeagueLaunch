68bc7411e75c27a9fbdadc979790378e7a28c205
Eric Haskins <ehask71@gmail.com>
2013-09-06 15:56:07 -0400

    forgetpwd

---
+++ app/Controller/RandomteamsController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CakePHP RandomteamsController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class RandomteamsController extends AppController {
+    public $name="Randomteams";
+    
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+    
+    public function index(){
+        
+    }
+    
+}
