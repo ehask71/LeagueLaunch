f336be20b7147fc4cbaf136c16aa95b403864f26
Eric Haskins <ehask71@gmail.com>
2013-09-06 22:02:00 -0400

    bPopup

--- app/Controller/RandomteamsController.php
+++ app/Controller/RandomteamsController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP RandomteamsController
  * @author Eric
@@ -7,28 +8,46 @@ App::uses('AppController', 'Controller');
 
 class RandomteamsController extends AppController {
 
-    public $uses = array('Divisions','Team','PlayersToSeasons','Players');
+    public $uses = array('Divisions', 'Team', 'PlayersToSeasons', 'Players');
+
     public function beforeFilter() {
         parent::beforeFilter();
     }
-    
-    public function admin_index(){
-        $divisions = $this->Divisions->find('all',array(
+
+    public function admin_index() {
+        $divisions = $this->Divisions->find('all', array(
             'conditions' => array(
                 'Divisions.active' => 1,
                 'Divisions.site_id' => Configure::read('Settings.site_id')
             ),
             'contain' => array(
-                'Team'=> array(
+                'Team' => array(
                     'Team.active' => 1
-                ))
-        ));
-        
+            ))
+                ));
+
+        foreach ($divisions AS $div) {
+            if (count($div['Team']) > 0) {
+                $players = $this->PlayersToSeasons->find('all', array(
+                    'conditions' => array(
+                        'PlayersToSeasons.division_id' => $div[Divisions][division_id],
+                        'PlayersToSeasons.site_id' => $div[Divisions][site_id],
+                        'PlayersToSeasons.haspaid' => 1
+                     )
+                        ));
+
+                if (count($players)>0){
+                    $divisions['players'] = $players;
+                }
+            }
+        }
+
+
         $this->set(compact('divisions'));
     }
-    
-    public function admin_generate($id){
+
+    public function admin_generate($id) {
         
     }
-    
+
 }
