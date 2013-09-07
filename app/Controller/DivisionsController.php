b52ab704e9e408f25b66a303ea999264a93aadbd
Eric Haskins <ehask71@gmail.com>
2013-09-07 19:02:12 -0400

    bPopup

--- app/Controller/DivisionsController.php
+++ app/Controller/DivisionsController.php
@@ -11,74 +11,85 @@ class DivisionsController extends AppController {
     var $uses = array('Divisions');
 
     public function beforeFilter() {
-	parent::beforeFilter();
+        parent::beforeFilter();
         $this->Auth->allow('index');
     }
 
     public function index() {
-	$this->paginate = array(
-	    'conditions' => array(
-		'Divisions.site_id' => Configure::read('Settings.site_id')
-	    ) 
-	);
-	$divisions = $this->paginate('Divisions');
-	$this->set(compact('divisions'));
+        $this->paginate = array(
+            'conditions' => array(
+                'Divisions.site_id' => Configure::read('Settings.site_id')
+            )
+        );
+        $divisions = $this->paginate('Divisions');
+        $this->set(compact('divisions'));
     }
 
     public function admin_index() {
-	if ($this->request->is('post')) {
-	    if ($this->Divisions->divisionValidate()) {
-		$this->Divisions->save($this->request->data, false);
-		$this->Session->setFlash(__('The Division was Added!'),'default',array('class'=>'alert succes_msg'));
-		$this->redirect('/admin/divisions');
-	    }
-	}
-	$this->paginate = array(
-	    'conditions' => array(
-		'Divisions.site_id' => Configure::read('Settings.site_id')
-	    ) 
-	);
-	$divisions = $this->paginate('Divisions');
-	$this->set(compact('divisions'));
-        $this->set('divdropdown',$this->Divisions->getDivisionsDropdown());
+        if ($this->request->is('post')) {
+            if ($this->Divisions->divisionValidate()) {
+                $this->Divisions->save($this->request->data, false);
+                $this->Session->setFlash(__('The Division was Added!'), 'default', array('class' => 'alert succes_msg'));
+                $this->redirect('/admin/divisions');
+            }
+        }
+        $this->paginate = array(
+            'conditions' => array(
+                'Divisions.site_id' => Configure::read('Settings.site_id')
+            )
+        );
+        $divisions = $this->paginate('Divisions');
+        $this->set(compact('divisions'));
+        $this->set('divdropdown', $this->Divisions->getDivisionsDropdown());
     }
-    
-    public function admin_delete($id){
-	$this->Divisions->id = $id;
-	if(!$this->Divisions->exists()){
-	    $this->Session->setFlash(__('Division doesn\'t Exist'),'default',array('class'=>'alert error_msg'));
-	    $this->redirect('/admin/divisions');
-	}
-	if($this->Divisions->delete()){
-	    $this->Session->setFlash(__('Division Deleted'),'default',array('class'=>'alert succes_msg'));
-	    $this->redirect('/admin/divisions');
-	}
-	$this->Session->setFlash(__('Unable To Delete Division'),'default',array('class'=>'alert error_msg'));
-	$this->redirect('/admin/divisions');
+
+    public function admin_delete($id) {
+        $this->Divisions->id = $id;
+        if (!$this->Divisions->exists()) {
+            $this->Session->setFlash(__('Division doesn\'t Exist'), 'default', array('class' => 'alert error_msg'));
+            $this->redirect('/admin/divisions');
+        }
+        if ($this->Divisions->delete()) {
+            $this->Session->setFlash(__('Division Deleted'), 'default', array('class' => 'alert succes_msg'));
+            $this->redirect('/admin/divisions');
+        }
+        $this->Session->setFlash(__('Unable To Delete Division'), 'default', array('class' => 'alert error_msg'));
+        $this->redirect('/admin/divisions');
     }
-    
-    public function admin_edit($id){
-	$this->Divisions->id = $id;
-	if(!$this->Divisions->exists()){
-	    $this->Session->setFlash(__('Division doesn\'t Exist'),'default',array('class'=>'alert error_msg'));
-	    $this->redirect('/admin/divisions');
-	}
-	if($this->request->isPut()){
-	    $this->Divisions->set($this->data);
-	    if ($this->Divisions->divisionValidate()) {
-		$this->Divisions->save($this->request->data, false);
-		$this->Session->setFlash(__('The Division was Updated!'),'default',array('class'=>'alert succes_msg'));
-		$this->redirect('/admin/divisions');
-	    }
-	} else {
-	    $div = $this->Divisions->read(null,$id);
-	    $this->request->data = null;
-	    if(!empty($div)){
-		$this->request->data = $div;
-	    }
-            $this->set('divdropdown',$this->Divisions->getDivisionsDropdown());
-	}
-	
+
+    public function admin_edit($id) {
+        $this->Divisions->id = $id;
+        if (!$this->Divisions->exists()) {
+            $this->Session->setFlash(__('Division doesn\'t Exist'), 'default', array('class' => 'alert error_msg'));
+            $this->redirect('/admin/divisions');
+        }
+        if ($this->request->isPut()) {
+            $this->Divisions->set($this->data);
+            if ($this->Divisions->divisionValidate()) {
+                $this->Divisions->save($this->request->data, false);
+                $this->Session->setFlash(__('The Division was Updated!'), 'default', array('class' => 'alert succes_msg'));
+                $this->redirect('/admin/divisions');
+            }
+        } else {
+            $div = $this->Divisions->read(null, $id);
+            $this->request->data = null;
+            if (!empty($div)) {
+                $this->request->data = $div;
+            }
+            $this->set('divdropdown', $this->Divisions->getDivisionsDropdown());
+        }
+    }
+
+    public function admin_playersteams($id, $season) {
+        $division = $this->Divisions->find('first', array(
+            'conditions' => array(
+                'Divisions.site_id' => Configure::read('Settings.site_id'),
+                'Divisions.season_id' => $season,
+                'Division.division_id' => $id
+            )
+                ));
+
+        $this->set(compact('division'));
     }
 
 }
