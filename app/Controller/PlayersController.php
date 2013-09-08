c686c9c25eb04d3eb400093e1c80af9bd08bdf01
Eric Haskins <ehask71@gmail.com>
2013-09-08 16:39:28 -0400

    bPopup

--- app/Controller/PlayersController.php
+++ app/Controller/PlayersController.php
@@ -8,13 +8,17 @@ App::uses('AppController', 'Controller');
 class PlayersController extends AppController {
     
     public $name = 'Players';
+    public $uses = array('Season');
     
     public function beforeFilter() {
 	parent::beforeFilter();
     }
     
-    public function index(){
+    public function admin_index(){
+        $seasons = $this->Season->getActiveSeasons();
         
+        $this->set(compact('seasons'));
+        $this->set('title_for_layout','Manage Players');
     }
     
 }
