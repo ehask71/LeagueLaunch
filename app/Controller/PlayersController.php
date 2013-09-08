4619661e4581b22c94b03a72211ae2166a2ced4c
Eric Haskins <ehask71@gmail.com>
2013-09-08 19:59:30 -0400

    bPopup

--- app/Controller/PlayersController.php
+++ app/Controller/PlayersController.php
@@ -23,14 +23,21 @@ class PlayersController extends AppController {
     
     public function admin_division($div,$season){
         $this->loadModel('PlayersToSeasons');
-        $players = $this->PlayersToSeasons->find('all',array(
+        /*$players = $this->PlayersToSeasons->find('all',array(
             'conditions' => array(
                 'PlayersToSeasons.division_id' => $div,
                 'PlayersToSeasons.haspaid' => 1,
                 'PlayersToSeasons.season_id' => $season,
                 'PlayersToSeasons.site_id' => Configure::read('Settings.site_id')
             )
-        ));
+        ));*/
+        $players = $this->PlayersToSeasons->query("
+            SELECT Players.player_id,Players.firstname,Players.lastname,Players.league_age,Divisions.name
+            FROM players_to_seasons PlayersToSeasons INNER JOIN players Players ON PlayersToSeasons.player_id = Players.player_id
+            INNER JOIN divisions Divisions ON PlayersToSeasons.division_id = Divisions.division_id
+            WHERE PlayersToSeasons.season_id = '".(int)$season."' AND PlayersToSeasons.division_id = '".(int)$div."'
+                AND PlayersToSeasons.site_id = '".Configure::read('Settings.site_id')."' AND PlayersToSeasons.haspaid = 1
+        ");
         
         $this->set('div',$div);
         $this->set('season',$season);
