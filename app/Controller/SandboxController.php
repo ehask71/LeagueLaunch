4c69910032f78abec5b7df641185fde3a4ad4c62
ehask71 <ehask71@gmail.com>
2013-09-14 15:33:37 -0400

    RoundRobin

--- app/Controller/SandboxController.php
+++ app/Controller/SandboxController.php
@@ -8,6 +8,8 @@ App::uses('AppController', 'Controller');
 class SandboxController extends AppController {
     
     public $name = 'Sandbox';
+    public $components = array('RoundRobin');
+    public $uses = array('Divisions','Team','Season','PlayersToSeasons');
     
     public function beforeFilter() {
 	parent::beforeFilter();
@@ -20,6 +22,37 @@ class SandboxController extends AppController {
     
     public function testiframe(){
         
+    }
+    
+    public function testschedule($id){
+	$this->autoRender = false;
+	$divisions = $this->Divisions->find('all',array(
+	    'conditions' => array(
+		'Divisions.active' => 1,
+		'Divisions.site_id' => Configure::read('Settings.site_id'),
+		'Divisions.season_id' => $id
+	    )
+	));
+	
+	foreach($divisions AS $div){
+	    $teams = $this->Team->find('all',array(
+		'conditions' => array(
+		    'Team.division_id' =>$div['Divisions']['division_id'],
+		    'Team.active' => 1,
+		    'Team.site_id' => Configure::read('Settings.site_id')
+		)
+	    ));
+	    
+	    $team_array = array();
+	    foreach ($teams AS $team){
+		$team_array[] = $team['Team']['name'];
+	    }
+	    $this->RoundRobin->roundrobin($team_array);
+	    echo $div['Divisions']['name'].'<br>';
+	    echo '<pre>';
+	    print_r($this->RoundRobin->games);
+	    echo '</pre>';
+	}
     }
     
 }
