579110c8839276c782952e5fbaa065306f042f1e
Eric Haskins <ehask71@gmail.com>
2013-10-07 10:29:57 -0400

    logo

--- app/Vendor/tcpdf/config/tcpdf_config.php
+++ app/Vendor/tcpdf/config/tcpdf_config.php
@@ -60,13 +60,13 @@
  * Default images directory.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-//define ('K_PATH_IMAGES', '');
+define ('K_PATH_IMAGES', '/home/demoleag/public_html/app/webroot/common/images/');
 
 /**
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-//define ('PDF_HEADER_LOGO', '');
+define ('PDF_HEADER_LOGO', 'logo-medium.png');
 
 /**
  * Header logo image width in user units.
