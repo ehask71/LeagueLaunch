8ac3a0ac7525ab6e4b1177ca09af07427078d4b5
EricMain <EricMain@EricMain-PC.home>
2013-10-15 22:46:52 -0400

    Initial Framework

--- app/Model/Fundraiser.php
+++ app/Model/Fundraiser.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP Fundraiser
  * @author Eric
@@ -6,7 +7,61 @@
 App::uses('AppModel', 'Model');
 
 class Fundraiser extends AppModel {
-    public $primaryKey = 'f_id';
-    
+
+    public $primaryKey = 'id';
+
+    public function validateFundraiser() {
+        $validate1 = array(
+            'name' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a Name')
+            ),
+            'description' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a Description')
+            ),
+            'type' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select the type')
+            ),
+            'start' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter a Start Date')
+            ),
+            'end' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter an End Date')
+            ),
+            'event_date' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please the Event Date')
+            ),
+            'event_location' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter the Event location')
+            ),
+            'provider' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please select a Provider')
+            ),
+            'create_product' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Select if you need a product Created')
+            )
+        );
+
+	$this->validate = $validate1;
+	return $this->validates();
+    }
+
 }
 
