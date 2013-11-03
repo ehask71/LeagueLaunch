47268e6ddcbbf8c09a77a545d86fa71e4b3c4b2a
EricMain <EricMain@EricMain-PC.home>
2013-11-03 00:06:07 -0400

    Template adjustments

---
+++ app/Model/Block.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * CakePHP Block
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class BlockModel extends Model {
+    
+    public $name = 'Block';
+    public $primaryKey = 'id';
+   
+
+}
