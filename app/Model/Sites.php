ca1cc13ea9d21b62d0c0b0fe1fd304c096bcec6c
EricMain <EricMain@EricMain-PC.home>
2013-11-14 18:33:31 -0500

    Add In Multiple Domains per account

--- app/Model/Sites.php
+++ app/Model/Sites.php
@@ -17,7 +17,7 @@ class Sites extends AppModel {
         //$sql = "SELECT * FROM sites WHERE domain = '$domain' AND isactive = 'yes'";
         $sql = "SELECT * FROM domains WHERE domain = '$domain'";
         $res = $this->query($sql);
-
+        mail('ehask71@gmail.com','Domains',print_r($res));
         if (count($res) > 0) {
             return $res;
         } else {
