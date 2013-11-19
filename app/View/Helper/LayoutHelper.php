2260b776889ebecfcba4423e396a27826e4ea2d5
ehask71 <ehask71@gmail.com>
2013-11-19 02:05:18 -0500

    added Layout Helper

---
+++ app/View/Helper/LayoutHelper.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CakePHP LayoutHelper
+ * @author Eric
+ */
+class LayoutHelper extends AppHelper {
+
+    public $helpers = array();
+    
+    public  $core
+
+    public function __construct(View $View, $settings = array()) {
+	parent::__construct($View, $settings);
+    }
+
+    public function beforeRender($viewFile) {
+	
+    }
+
+    public function afterRender($viewFile) {
+	
+    }
+
+    public function beforeLayout($viewLayout) {
+	
+    }
+
+    public function afterLayout($viewLayout) {
+	
+    }
+
+}
