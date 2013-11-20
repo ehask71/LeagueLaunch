dfe1c84dc7f074e9569b864b2ecfa5e357e6be5a
Eric Haskins <ehask71@gmail.com>
2013-11-19 19:20:13 -0500

    removing debug code

--- app/View/Helper/LayoutHelper.php
+++ app/View/Helper/LayoutHelper.php
@@ -1,30 +1,52 @@
 <?php
+
 /**
  * CakePHP LayoutHelper
  * @author Eric
  */
+App::uses('AppHelper', 'View/Helper');
+
 class LayoutHelper extends AppHelper {
 
     public $helpers = array();
 
     public function __construct(View $View, $settings = array()) {
-	parent::__construct($View, $settings);
+        parent::__construct($View, $settings);
     }
 
     public function beforeRender($viewFile) {
-	
+        
     }
 
     public function afterRender($viewFile) {
-	
+        
     }
 
     public function beforeLayout($viewLayout) {
-	
+        
     }
 
     public function afterLayout($viewLayout) {
-	
+        
+    }
+
+    public function isLoggedIn() {
+        if ($this->Session->check('Auth.User.id')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function sessionFlash() {
+        $messages = $this->Session->read('Message');
+        $output = '';
+        if (is_array($messages)) {
+            foreach (array_keys($messages) as $key) {
+                $output .= $this->Session->flash($key);
+            }
+        }
+        return $output;
     }
 
 }
