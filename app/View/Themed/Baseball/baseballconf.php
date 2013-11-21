785bf86b2b7da26f48eb7c2dbe9ebf776f95b843
Eric Haskins <ehask71@gmail.com>
2013-11-21 16:25:43 -0500

    payment form

--- app/View/Themed/Baseball/baseballconf.php
+++ app/View/Themed/Baseball/baseballconf.php
@@ -1,6 +1,9 @@
 <?php
 
 $config['Theme_Config'] = array(
-    'pages'=> array('index')
+    'layouts' => array('homepage' => 'default', '2-column' => 'twocol'),
+    'regions' => array('homepage' => array(
+	    'ad_top', 'top_content', 'bottom_content', 'bottom_ad', 'footer_1', 'footer_2', 'footer_3', 'footer_4')
+    )
 );
 
